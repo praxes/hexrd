29f7d9c88ac699ed2a1b74475b85873d747b8dd1
Donald Boyce <donald.e.boyce@gmail.com>
2012-01-02 18:29:42 -0500

    complete overhaul of valUnits

--- valUnits.py
+++ valUnits.py
@@ -23,289 +23,106 @@
 #   <http://www.gnu.org/licenses/>.
 #
 # DO-NOT-DELETE revisionify.end() 
-# for quantities with units: valUnits.py
 #
-# see also
-#	~/working/materials/mpsiForms.py
-# 	~/working/materials/iron/shared_c/matVals.py
-#	~/working/materials/iron/shared_c/make_cmat3MBTR_bdivk.py
-#
-# examples:
-#	from valUnits import *
-#
-#	# common units, like pressure, have short names in uTDict:
-#	c11 = valWUnit("c11", "p", 210., "GPa")
-#	c11.getVal("Mbar")
-#	c11.getVal(bdivk)
-#
-#	# oddball units are fine too:
-#	ab_c = valWUnit("ab_c", stressUN+"/"+temperatureUN, 1., "Mbar/degK")
-#	# but may not work properly with dictionary creation
+"""Module for associating units with scalar quantities
 
-# from scipy import *
-import os, sys, re
+This module has been modified from its original form
+by removing the call to the "units" executable and restricting
+the units to only those used by the heXRD package.
 
-if __name__ != '__main__':
-    debug = 0
+"""
 
-unitsCommand = 'units'
-if os.path.exists('/sw/bin/units'):
-    unitsCommand = '/sw/bin/units'
-#
-import fileUtil
-haveGenerics = False
-try:
-    unitsVersion = map(int, fileUtil.getFromPipe('%s --version' % (unitsCommand), werr=True).split()[-1].split('.'))
-    haveGenerics = unitsVersion[0] >= 1 and unitsVersion[1] >= 85
-except:
-    'do not bother with fall-back'
+import math
+
+__all__ = ['valWUnit', 'toFloat', 'valWithDflt']
 
 # centralized unit types; chosen to have a match in the units command
-forceUN = "FORCE"
-areaInvUN = "1/AREA"
 energyUN = "ENERGY"
-timeUN = "TIME"
-massUN = "MASS"
 lengthUN = "LENGTH"
 angleUN = "ANGLE"
-volumeUN = "VOLUME"
-temperatureUN = "TEMPERATURE"
-rateUN = "FREQUENCY" # strainrate
-stressUN = "STRESS"
-pressureUN = "STRESS"
-densityUN = "DENSITY"
-heatCapMUN = "ENERGY/(MASS TEMPERATURE)"
-heatCapUN = "ENERGY/(VOLUME TEMPERATURE)"
-activationUN = "TEMPERATURE/STRESS"
-velocityUN = "LENGTH/TIME"
-nullUN = "none"
 #
 # mapping to centralized unit types, for convenience
+#
 uTDict = {
-    "areaInv"    : areaInvUN,
     "length"     : lengthUN,
     "angle"      : angleUN,
     "energy"     : energyUN,
-    "volume"     : volumeUN,
-    "stress"     : stressUN,
-    "pressure"   : stressUN,
-    "pres"       : stressUN,
-    "p"          : stressUN,
-    "strainrate" : rateUN,
-    "time"       : timeUN,
-    "timerate"   : rateUN,
-    "frequency"  : rateUN,
-    "temperature" : temperatureUN,
-    "rate"       : rateUN,
-    "heatCapM"   : heatCapMUN,
-    "heatCap"    : heatCapUN,
-    "density"    : densityUN,
-    "velocity"   : velocityUN,
-    "none"       : nullUN,
-    "null"       : nullUN
     }
 
-# for convenience
-GPa = "GPa"
-permicrosecond = "1/microsecond"
 
-def fillUnitSystem(uS):
-    def toPow(this,pow):
-        val = "(%s)^(%g)" % (this,pow)
-        return val
-    def divBy(a,b):
-        'a/b'
-        val = "(%s)/(%s)" % (a,b)
-        return val
-    uS[nullUN] = nullUN
-    if (not uS.has_key(volumeUN)):
-        uS[volumeUN] = toPow(uS[lengthUN],3)
-    if (not uS.has_key(densityUN)):
-        uS[densityUN] = divBy(uS[massUN],uS[volumeUN])
-    if (not uS.has_key(forceUN)):
-        uS[forceUN] = divBy(uS[massUN]+" "+uS[lengthUN], toPow(uS[timeUN],2))
-    if (not uS.has_key(energyUN)):
-        uS[energyUN] = divBy(uS[massUN]+" "+toPow(uS[lengthUN],2), toPow(uS[timeUN],2))
-    if (not uS.has_key(stressUN)):
-        uS[stressUN] = divBy(uS[massUN], toPow(uS[timeUN],2)+" "+uS[lengthUN])
-    if (not uS.has_key(rateUN)):
-        uS[rateUN] = divBy("1", uS[timeUN])
-    if (not uS.has_key(heatCapMUN)):
-        uS[heatCapMUN] = divBy(uS[energyUN], uS[massUN]+" "+uS[temperatureUN])
-    if (not uS.has_key(heatCapUN)):
-        uS[heatCapUN] =  divBy(uS[energyUN], uS[volumeUN]+" "+uS[temperatureUN])
-    if (not uS.has_key(activationUN)):
-        uS[activationUN] = divBy(uS[temperatureUN], uS[stressUN])
-    if (not uS.has_key(areaInvUN)):
-        uS[areaInvUN] = toPow(uS[lengthUN],-2)
-    if (not uS.has_key(velocityUN)):
-        uS[velocityUN] = divBy(uS[lengthUN],uS[timeUN])
-    return
+class UNames(object):
+    """Units used in this module"""
+    degrees = 'degrees'
+    radians = 'radians'
 
-# unit systems
-bdivk = {
-    timeUN         : "microsecond",
-    massUN         : "gram",
-    lengthUN       : "centimeter",
-    temperatureUN  : "degK",
-    stressUN       : "Mbar"
-    }
-fillUnitSystem(bdivk)
-cm_g_mus = bdivk
-#
-mum_g_ms = {
-    timeUN         : "millisecond",
-    massUN         : "gram",
-    lengthUN       : "micrometer",
-    temperatureUN  : "degK"
-    }
-fillUnitSystem(mum_g_ms)
-#
-mum_kg_s = {
-    timeUN         : "second",
-    massUN         : "kilogram",
-    lengthUN       : "micrometer",
-    temperatureUN  : "degK"
-    }
-fillUnitSystem(mum_kg_s)
-#
-# for mm_g_ms:
-#	stress ends up being in GPa
-mm_g_ms = {
-    timeUN         : "millisecond",
-    massUN         : "gram",
-    lengthUN       : "millimeter",
-    temperatureUN  : "degK"
-    }
-fillUnitSystem(mm_g_ms)
-#
-mm_kg_ms = {
-    timeUN         : "millisecond",
-    massUN         : "kilogram",
-    lengthUN       : "millimeter",
-    temperatureUN  : "degK"
-    }
-fillUnitSystem(mm_kg_ms)
-#
-# Aaron Wemhoff's units:
-#  Unit of time = 10^-10 s
-#  Unit of distance = 10^-6 m
-#  Unit of mass = 10^-12 g.
-wemhoff_uS = { # mum_picog_1e-10s
-    timeUN         : "0.1 nanosecond",
-    massUN         : "picogram",
-    lengthUN       : "micrometer",
-    temperatureUN  : "degK"
-    }
-fillUnitSystem(wemhoff_uS)
-# checks:
-# valWUnit('pres', stressUN, 1.0, 'Mbar').getVal(wemhoff_uS)
-#	-> 1.0
-# valWUnit('dens', densityUN, 1.0, 'gram/(cm^3)').getVal(wemhoff_uS)
-# 	-> 1.0
-um_pg_ws = wemhoff_uS
+    m = 'm'
+    mm = 'mm'
+    meter = 'meter'
+    angstrom = 'angstrom'
 
-def callUnits(uV, toUnit):
-    negative = False
-    val = uV.value
-    if uV.value < 0:
-        negative = True
-        val = -val
-    #
-    # Command needs double quotes on windows
-    #
-    #command = "%s '%s %s' '%s'" % (unitsCommand, val, uV.unit, toUnit)
-    command = '%s "%s %s" "%s"' % (unitsCommand, val, uV.unit, toUnit)
-    unitsPipe = os.popen(command)
-    tmp = unitsPipe.readline()
-    unitRetVal = None
-    while tmp:
-        if debug > 1 :
-            print "tmp:", tmp
-        tmpb = re.split("\*",tmp)
-        if len(tmpb) > 1:
-            unitRetVal = float(tmpb[1])
-        tmp = unitsPipe.readline()
-    status = unitsPipe.close()
-    if status != None:
-        print >> sys.stderr, "error %s from units command %s" % (status,command)
-        raise RuntimeError, "unrecoverable error"
-    if unitRetVal is None:
-        print >> sys.stderr, "unitRelVal not set from units command %s" % (command)
-        raise RuntimeError, "unrecoverable error"
-    if negative:
-        unitRetVal = -unitRetVal
-    return unitRetVal
+    J = 'J'
+    
+    pass
 
-class valWUnit:
-    "value with units"
-    def __init__(self, *args):
-        if len(args) == 4:
-            name     = args[0]
-            unitType = args[1]
-            value    = args[2]
-            unit     = args[3]
-        elif len(args) == 3:
-            name     = args[0]
-            unitType = args[1]
-            value    = args[2]
-            if unitType == nullUN:
-                unit     = nullUN
-            else:
-                print >> sys.stderr, "unable to figure out arguments"
-                raise RuntimeError, "unrecoverable error"
-        else:
-            print >> sys.stderr, "unable to figure out arguments"
-            raise RuntimeError, "unrecoverable error"
+cv_dict = {
+    (UNames.degrees, UNames.radians): math.pi/180.0,
+    (UNames.radians, UNames.degrees): 180/math.pi,
+    
+    (UNames.m, UNames.mm):  1.0e3,
+    (UNames.m, UNames.meter):  1.0,
+    (UNames.m, UNames.angstrom):  1.0e10,
+    
+    (UNames.meter, UNames.mm):  1.0e3,
+    (UNames.meter, UNames.m):  1.0,
+    (UNames.meter, UNames.angstrom):  1.0e10,
+    
+    (UNames.mm, UNames.m):  1.0e-3,
+    (UNames.mm, UNames.meter):  1.0e-3,
+    (UNames.mm, UNames.angstrom):  1.0e7,
+    
+    (UNames.angstrom, UNames.m):  1.0e-10,
+    (UNames.angstrom, UNames.meter):  1.0e-10,
+    (UNames.angstrom, UNames.mm):  1.0e-7,
+    
+    }
+
+class valWUnit(object):
+    "Value with units"""
+    def __init__(self, name, unitType, value, unit):
+        """Initialization
+
+        INPUTS
+        name
+           (str) name of the item
+        unitType
+           (str) class of units, e.g. 'length', 'angle'
+        value
+           (float) numerical value 
+        unit
+           (str) name of unit
+"""
         self.name = name
         if uTDict.has_key(unitType):
             self.uT   = uTDict[unitType]
         else:
             # trust that unitType is correct -- may be a combined type
             self.uT   = unitType
+            
         self.value = value
         self.unit = unit
-        if (not unitType == nullUN):
-            # call callUnits to make sure that unit is of unitType
-            if haveGenerics:
-                dummy = callUnits(self, self.uT)
-            else:
-                dummy = callUnits(self, self.unit)
-            # if this did not raise an exception, the unit is of the right type
-    def __repr__(self):
-        return 'valWUnit("%s","%s",%s,"%s")' % (self.name, self.uT, self.value, self.unit)
+        #
+        #  Original checked if unit is of unitType
+        #
+        pass # end init
+            
     def __str__(self):
-        return "%s %s" % (self.value, self.unit)
-    def isLength(self):
-        retval = self.uT == uTDict['length']
-        return retval
-    def isAngle(self):
-        retval = self.uT == uTDict['angle']
-        return retval
-    def isEnergy(self):
-        retval = self.uT == uTDict['energy']
-        return retval
-    def getVal(self, asUnit):
-        import string
-        "asUnit can be a string or a unit system dictionary"
-        if isinstance(asUnit, dict):
-            if asUnit.has_key(self.uT):
-                toUnit = asUnit[self.uT]
-            else:
-                "attempt to use dictionary to make toUnit"
-                toUnit = self.uT
-                for item in asUnit.iteritems():
-                    toUnit = string.replace(toUnit, item[0], item[1])
-                #print >> sys.stderr, "key not found in uTDict %s" % self.uT
-                #raise RuntimeError, "unrecoverable error"
-        else:
-            toUnit = asUnit
-        
-        if toUnit == nullUN:
-            return self.value
-        
-        unitRetVal = callUnits(self, toUnit)
-        return unitRetVal
+        tmpl = """item named "%s" representing %g %s""" 
+        return tmpl % (self.name, self.value, self.unit)
+    
+    def __repr__(self):
+        tmpl = 'valWUnit("%s","%s",%s,"%s")' 
+        return tmpl % (self.name, self.uT, self.value, self.unit)
+    
     def __mul__(self, other):
         if isinstance(other, float):
             new = valWUnit(self.name, self.uT, self.value*other, self.unit)
@@ -319,7 +136,8 @@ class valWUnit:
             # really need to put in here something to resolve new.uT
             return new
         else:
-            raise RuntimeError, "mul with unsupported operand"
+            raise RuntimeError("mul with unsupported operand")
+        
     def __add__(self, other):
         if isinstance(other, float):
             new = valWUnit(self.name, self.uT, self.value + other, self.unit)
@@ -328,7 +146,8 @@ class valWUnit:
             new = valWUnit(self.name, self.uT, self.value + other.getVal(self.unit), self.unit)
             return new
         else:
-            raise RuntimeError, "add with unsupported operand"
+            raise RuntimeError("add with unsupported operand")
+        
     def __sub__(self, other):
         if isinstance(other, float):
             new = valWUnit(self.name, self.uT, self.value - other, self.unit)
@@ -337,42 +156,68 @@ class valWUnit:
             new = valWUnit(self.name, self.uT, self.value - other.getVal(self.unit), self.unit)
             return new
         else:
-            raise RuntimeError, "add with unsupported operand"
+            raise RuntimeError("add with unsupported operand")
 
-def makeUnitValDict(vUList, unitSystem):
-    "make a dictionary with values and units, given a unit system"
-    dict = {}
-    for vU in vUList:
-        dict[vU.name] = vU.getVal(unitSystem)
-        dict[vU.name + "_unit"] = unitSystem[vU.uT]
-    return dict
+    def _convert(self, toUnit):
+        """Convert unit"""
+        if self.unit == toUnit:
+            return self.value
+        #
+        #  Needs conversion
+        #
+        try:
+            return cv_dict[(self.unit, toUnit)]*self.value
+        except:
+            raise RuntimeError("Unit conversion not recognized")
+        
+    def isLength(self):
+        """Return true if quantity is a length"""
+        retval = self.uT == uTDict['length']
+        return retval
+    def isAngle(self):
+        """Return true if quantity is an angle"""
+        retval = self.uT == uTDict['angle']
+        return retval
+    def isEnergy(self):
+        """Return true if quantity  is an energy"""
+        retval = self.uT == uTDict['energy']
+        return retval
+    
+    def getVal(self, toUnit):
+        """Return value in requested units
 
-def makeValDict(vUList, unitSystem, prefix=None):
-    "make a dictionary with values, given a unit system"
-    dict = {}
-    for vU in vUList:
-        if prefix:
-            dict[prefix + vU.name] = vU.getVal(unitSystem)
-        else:
-            dict[vU.name] = vU.getVal(unitSystem)
-    return dict
-
-def getConversion(uS, unitType, unit):
-    tempValWUnit = valWUnit('temp',
-                            unitType,
-                            1.0,
-                            unit)
-    conversion = tempValWUnit.getVal(uS)
-    return conversion
-
-def toFloatScalar(val, unitName):
-    if hasattr(val, 'getVal'):
-        retval = val.getVal(unitName)
-    else:
-        retval = val
-    return retval
+        INPUTS
+        
+        toUnit
+           (str) requested unit for output
+"""
+        return self._convert(toUnit)
     
 def toFloat(val, unitName):
+    """Return the raw value of the object
+
+    INPUTS
+    
+    val
+       (float|valWUnit) object with value
+    unitName
+       (str) name of unit
+
+    This function returns the raw value of the object, ignoring the
+    unit, if it is numeric or converts it to the requested units and
+    returns the magnitude if it is a valWUnit instance.
+
+    For example:
+    
+    >>> print toFloat(1.1, 'radians')
+    1.1
+    >>> v = valWUnit('vee', 'angle', 1.1, 'radians')
+    >>> print toFloat(v, 'degrees')
+    63.0253574644
+        
+    """
+    toFloatScalar = lambda v, u: v.getVal(u) if hasattr(v, 'getVal') else v
+    
     if hasattr(val, '__len__'):
         retval = map(lambda x: toFloatScalar(x, unitName), val)
     else:
@@ -380,6 +225,8 @@ def toFloat(val, unitName):
     return retval
 
 def valWithDflt(val, dflt, toUnit=None):
+    """Return value or default value"""
+    
     retval = val
     if retval is None:
         retval = dflt
@@ -387,9 +234,32 @@ def valWithDflt(val, dflt, toUnit=None):
         retval = toFloat(retval, toUnit)
     return retval
     
+if __name__ == '__main__':
+    #
+    #  doc testing
+    #
+    import doctest
+    print "running doctest"
+    doctest.testmod()
+    #
+    #  other tests
+    #
+    def testConversions():
+        print '===== Testing unit conversions ...'
+        print '..... angles:'
+        v = valWUnit('180d', 'angle', 180.0, 'degrees')
+        print v
+        print '   in degrees:', v.getVal('degrees')
+        print '   in radians: ', v.getVal('radians')
 
-'''
-import units
-bar = units.scaled_unit("br","Pa",1.0e5) # not happy with 'bar' for some reason
-Pa = units.unit('Pa')
-'''
+        print '..... lengths:'
+        ulist = ['m', 'mm', 'meter', 'angstrom']
+        v = valWUnit('one meter', 'length', 1.0, 'meter')
+        print v
+        for u in ulist:
+            print '   in ', u, ': ', v.getVal(u)
+        return
+
+    testConversions()
+    
+    pass # end of tests
