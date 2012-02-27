777f1590c3adf6085ed9cf27462fc8161a0c2111
Donald E. Boyce <deb14@cornell.edu>
2012-02-27 10:41:52 -0500

    updated copyright headers

--- quadrature/qloc3dData.py
+++ quadrature/qloc3dData.py
@@ -1,27 +1,28 @@
-# DO-NOT-DELETE revisionify.begin() 
-#
-#   Copyright (c) 2007-2009 Lawrence Livermore National Security,
-#   LLC. Produced at the Lawrence Livermore National Laboratory (Nathan
-#   Barton <barton22@llnl.gov>) CODE-OCEC-08-104.
-#   
-#   Please also read the file NOTICES.
-#   
-#   This file is part of the mdef package (version 0.2) and is
-#   free software: you can redistribute it and/or modify it under the
-#   terms of the GNU Lesser General Public License as published by the
-#   Free Software Foundation, either version 3 of the License, or (at your
-#   option) any later version.
-#   
-#   This program is distributed in the hope that it will be useful, but
-#   WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#   Lesser General Public License for more details.
-#   
-#   A copy of the GNU Lesser General Public License may be found in the
-#   file NOTICES. If this file is missing, see
-#   <http://www.gnu.org/licenses/>.
-#
-# DO-NOT-DELETE revisionify.end() 
+# ============================================================
+# Copyright (c) 2007-2012, Lawrence Livermore National Security, LLC. 
+# Produced at the Lawrence Livermore National Laboratory. 
+# Written by Joel Bernier <bernier2@llnl.gov> and others. 
+# LLNL-CODE-529294. 
+# All rights reserved.
+# 
+# This file is part of HEXRD. For details, see https://github.com/joelvbernier/hexrd.
+# 
+# Please also see the file LICENSE.
+# 
+# This program is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License (as published by the Free Software
+# Foundation) version 2.1 dated February 1999.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the 
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program (see file LICENSE); if not, write to
+# the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA 02111-1307 USA or visit <http://www.gnu.org/licenses/>.
+# ============================================================
 import numpy as num
 
 'data'
