90ef51a6b69db117ae58bee537e35dd170b089cf
Donald Boyce <deb14@cornell.edu>
2012-07-30 10:00:07 -0500

    added interactors for indexing options

--- hexrd/GUI/indexPanel.py
+++ hexrd/GUI/indexPanel.py
@@ -78,12 +78,14 @@ class indexPanel(wx.Panel):
         """Add interactors"""
 
         self.sz_titlebar = makeTitleBar(self, 'Indexing')
-        self.method_cho = wx.Choice(self, wx.NewId(), choices=self.INDEX_CHOICES)
+        self.method_cho = wx.Choice(self, wx.NewId(),
+                                    choices=self.INDEX_CHOICES)
+        self.method_cho.SetSelection(self.IND_FIBER)
         self.run_but  = wx.Button(self, wx.NewId(), 'Run Indexer')
 
         self.fiber_pan = FiberSearchPanel(self, wx.NewId())
         self.gspot_pan = GrainSpotterPanel(self, wx.NewId())
-        
+
         return
 
     def __makeBindings(self):
@@ -168,6 +170,42 @@ class FiberSearchPanel(wx.Panel):
         self.tbarSizer = makeTitleBar(self, 'Fiber Search Options', 
                                       color=WP.BG_COLOR_PANEL1_TITLEBAR)
 
+        # checkboxes
+        
+        self.friedel_cbox = wx.CheckBox(self, wx.NewId(), 'Friedel Only')
+        self.claims_cbox = wx.CheckBox(self, wx.NewId(), 'Preserve Claiims')
+        self.refine_cbox  = wx.CheckBox(self, wx.NewId(), 'Do Refinement')
+        self.multi_cbox  = wx.CheckBox(self, wx.NewId(), 'Use Multiprocessing')
+
+        # value boxes
+        
+        self.etol_lab = wx.StaticText(self, wx.NewId(), 'Eta Tolerance',
+                                      style=wx.ALIGN_CENTER)
+        self.etol_txt = wx.TextCtrl(self, wx.NewId(), value='',
+                                    style=wx.RAISED_BORDER)
+        
+        self.otol_lab = wx.StaticText(self, wx.NewId(), 'Omega Tolerance',
+                                      style=wx.ALIGN_CENTER)
+        self.otol_txt = wx.TextCtrl(self, wx.NewId(), value='',
+                                    style=wx.RAISED_BORDER)
+        
+        self.steps_lab = wx.StaticText(self, wx.NewId(), 'Number of Steps',
+                                      style=wx.ALIGN_CENTER)
+        self.steps_spn = wx.SpinCtrl(self, wx.NewId(),
+                                     min=10, max=360000, initial=360)
+
+        label = 'Minimum Completeness'
+        self.comp_lab = wx.StaticText(self, wx.NewId(), label,
+                                      style=wx.ALIGN_CENTER)
+        self.comp_txt = wx.TextCtrl(self, wx.NewId(), value='0.5',
+                                    style=wx.RAISED_BORDER)
+
+        label = 'Minimum Percent Claimed'
+        self.claim_lab = wx.StaticText(self, wx.NewId(), label,
+                                      style=wx.ALIGN_CENTER)
+        self.claim_txt = wx.TextCtrl(self, wx.NewId(), value='50',
+                                    style=wx.RAISED_BORDER)
+
         return
 
     def __makeBindings(self):
@@ -176,9 +214,37 @@ class FiberSearchPanel(wx.Panel):
 
     def __makeSizers(self):
 	"""Lay out the interactors"""
-	
+        # checkbox sizer
+        
+	self.cbsizer = wx.BoxSizer(wx.VERTICAL) # checkboxes
+        self.cbsizer.Add(self.friedel_cbox, 0, wx.ALIGN_LEFT)
+        self.cbsizer.Add(self.claims_cbox, 0, wx.ALIGN_LEFT)
+        self.cbsizer.Add(self.refine_cbox, 0, wx.ALIGN_LEFT)
+        self.cbsizer.Add(self.multi_cbox, 0, wx.ALIGN_LEFT)
+
+        # value controls
+        
+        nrow = 0; ncol = 2; padx = pady = 5
+	self.valsizer = wx.FlexGridSizer(nrow, ncol, padx, pady) 
+	self.valsizer.AddGrowableCol(1, 1)
+	#self.valsizer.AddGrowableRow(num, proportion)
+	self.valsizer.Add(self.etol_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.etol_txt, 0, wx.EXPAND|wx.ALIGN_LEFT)
+	self.valsizer.Add(self.otol_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.otol_txt, 0, wx.EXPAND|wx.ALIGN_LEFT)
+	self.valsizer.Add(self.steps_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.steps_spn, 0, wx.EXPAND|wx.ALIGN_LEFT)
+	self.valsizer.Add(self.comp_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.comp_txt, 0, wx.EXPAND|wx.ALIGN_LEFT)
+	self.valsizer.Add(self.claim_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.claim_txt, 0, wx.EXPAND|wx.ALIGN_LEFT)
+
+        # Main Sizer
+        
 	self.sizer = wx.BoxSizer(wx.VERTICAL)
 	self.sizer.Add(self.tbarSizer, 0, wx.EXPAND|wx.ALIGN_CENTER)
+        self.sizer.Add(self.cbsizer, 0, wx.TOP, 10)
+        self.sizer.Add(self.valsizer, 0, wx.TOP, 10)
 
 	return
     #
@@ -232,6 +298,32 @@ class GrainSpotterPanel(wx.Panel):
         self.tbarSizer = makeTitleBar(self, 'Grain Spotter Options', 
                                       color=WP.BG_COLOR_PANEL1_TITLEBAR)
 
+        self.pfit_cbox = wx.CheckBox(self, wx.NewId(), 'Position Fit')
+        
+        label = 'Minimum Completeness'
+        self.comp_lab = wx.StaticText(self, wx.NewId(), label,
+                                      style=wx.ALIGN_CENTER)
+        self.comp_txt = wx.TextCtrl(self, wx.NewId(), value='0.5',
+                                    style=wx.RAISED_BORDER)
+
+        label = 'Minimum Fraction of G-Vectors'
+        self.fracG_lab = wx.StaticText(self, wx.NewId(), label,
+                                      style=wx.ALIGN_CENTER)
+        self.fracG_txt = wx.TextCtrl(self, wx.NewId(), value='0.5',
+                                    style=wx.RAISED_BORDER)
+
+        label = 'Sigmas'
+        self.sigmas_lab = wx.StaticText(self, wx.NewId(), label,
+                                      style=wx.ALIGN_CENTER)
+        self.sigmas_txt = wx.TextCtrl(self, wx.NewId(), value='2.0',
+                                    style=wx.RAISED_BORDER)
+
+        self.trials_lab = wx.StaticText(self, wx.NewId(), 'Number of Trials',
+                                      style=wx.ALIGN_CENTER)
+        self.trials_spn = wx.SpinCtrl(self, wx.NewId(),
+                                     min=1000, max=1000000, initial=100000)
+
+
         return
 
     def __makeBindings(self):
@@ -240,9 +332,25 @@ class GrainSpotterPanel(wx.Panel):
 
     def __makeSizers(self):
 	"""Lay out the interactors"""
+        # value controls
+        
+        nrow = 0; ncol = 2; padx = pady = 5
+	self.valsizer = wx.FlexGridSizer(nrow, ncol, padx, pady) 
+	self.valsizer.AddGrowableCol(1, 1)
+	#self.valsizer.AddGrowableRow(num, proportion)
+	self.valsizer.Add(self.comp_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.comp_txt, 0, wx.EXPAND|wx.ALIGN_LEFT)
+	self.valsizer.Add(self.fracG_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.fracG_txt, 0, wx.EXPAND|wx.ALIGN_LEFT)
+	self.valsizer.Add(self.sigmas_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.sigmas_txt, 0, wx.EXPAND|wx.ALIGN_LEFT)
+	self.valsizer.Add(self.trials_lab, 0, wx.EXPAND|wx.ALIGN_RIGHT)
+	self.valsizer.Add(self.trials_spn, 0, wx.EXPAND|wx.ALIGN_LEFT)
 	
 	self.sizer = wx.BoxSizer(wx.VERTICAL)
 	self.sizer.Add(self.tbarSizer, 0, wx.EXPAND|wx.ALIGN_CENTER)
+	self.sizer.Add(self.pfit_cbox, 0, wx.TOP, 10)
+        self.sizer.Add(self.valsizer, 0, wx.TOP, 10)
 
 	return
     #
