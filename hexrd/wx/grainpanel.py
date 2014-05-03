7211c3127988ae331b1ee7dd745593e1c78f2ada
Joel Bernier <joelvbernier@me.com>
2014-05-03 14:33:24 -0700

    fixed broken grainPanel
    debris from botched merge

--- hexrd/wx/grainpanel.py
+++ hexrd/wx/grainpanel.py
@@ -1,25 +1,25 @@
 #! /usr/bin/env python
 # ============================================================
-# Copyright (c) 2012, Lawrence Livermore National Security, LLC. 
-# Produced at the Lawrence Livermore National Laboratory. 
-# Written by Joel Bernier <bernier2@llnl.gov> and others. 
-# LLNL-CODE-529294. 
+# Copyright (c) 2012, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by Joel Bernier <bernier2@llnl.gov> and others.
+# LLNL-CODE-529294.
 # All rights reserved.
-# 
+#
 # This file is part of HEXRD. For details on dowloading the source,
 # see the file COPYING.
-# 
+#
 # Please also see the file LICENSE.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it under the
 # terms of the GNU Lesser General Public License (as published by the Free Software
 # Foundation) version 2.1 dated February 1999.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY 
-# or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the 
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this program (see file LICENSE); if not, write to
 # the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
@@ -31,43 +31,22 @@
 import wx
 
 from hexrd.wx.guiconfig import WindowParameters as WP
-from hexrd.wx.guiutil   import makeTitleBar
+from hexrd.wx.guiutil import makeTitleBar, callJoel
 #
-# ---------------------------------------------------CLASS:  GrainPanel
+# ---------------------------------------------------CLASS:  grainPanel
 #
-class GrainPanel(wx.Panel):
-    """GrainPanel """
+class grainPanel(wx.Panel):
+    """grainPanel """
     def __init__(self, parent, id, **kwargs):
-<<<<<<< HEAD
-	"""Constructor for GrainPanel."""
-	#
-	wx.Panel.__init__(self, parent, id, **kwargs)
-=======
         """Constructor for grainPanel."""
         #
         wx.Panel.__init__(self, parent, id, **kwargs)
->>>>>>> refactor
         self.SetBackgroundColour(WP.BG_COLOR_PANEL)
         #
         #  Data
         #
 
         #
-<<<<<<< HEAD
-	#  Window Objects.
-	#
-        self.__make_objects()
-	#
-	#  Bindings.
-	#
-	self.__make_bindings()
-	#
-	#  Sizing.
-	#
-	self.__make_sizers()
-	#
-	self.SetAutoLayout(True)
-=======
         #  Window Objects.
         #
         self.__makeObjects()
@@ -81,42 +60,30 @@ class GrainPanel(wx.Panel):
         self.__makeSizers()
         #
         self.SetAutoLayout(True)
->>>>>>> refactor
         self.SetSizerAndFit(self.sizer)
         #
         return
     #
     # ============================== Internal Methods
     #
-    def __make_objects(self):
+    def __makeObjects(self):
         """Add interactors"""
 
-        self.sz_titlebar = makeTitleBar(self, 'Grains')
-        self.glist_pan = GrainListSubPanel(self, wx.NewId())
-        self.refine_pan = RefinementSubPanel(self, wx.NewId())
+        self.sz_titlebar = makeTitleBar(self, 'Indexing')
+        self.hpage = callJoel(self)
 
         return
 
-    def __make_bindings(self):
+    def __makeBindings(self):
         """Bind interactors"""
         return
 
-<<<<<<< HEAD
-    def __make_sizers(self):
-	"""Lay out the interactors"""
-	
-	self.sizer = wx.BoxSizer(wx.VERTICAL)
-	self.sizer.Add(self.sz_titlebar, 0, wx.EXPAND|wx.ALIGN_CENTER)
-	self.sizer.Add(self.glist_pan,   1, wx.EXPAND|wx.ALIGN_CENTER)
-	self.sizer.Add(self.refine_pan,  1, wx.EXPAND|wx.ALIGN_CENTER)
-=======
     def __makeSizers(self):
         """Lay out the interactors"""
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.sz_titlebar, 0, wx.EXPAND|wx.ALIGN_CENTER)
         self.sizer.Add(self.hpage,    1, wx.EXPAND|wx.ALIGN_CENTER)
->>>>>>> refactor
 
         return
     #
@@ -126,304 +93,7 @@ class GrainPanel(wx.Panel):
         """Update page"""
         return
 
-    
-    pass # end class
-#
-# -----------------------------------------------END CLASS:  GrainPanel
-# ---------------------------------------------------CLASS:  GrainListSubPanel
-#
-class GrainListSubPanel(wx.Panel):
-    """GrainListSubPanel """
-    def __init__(self, parent, id, **kwargs):
-	"""Constructor for GrainListSubPanel."""
-	#
-	wx.Panel.__init__(self, parent, id, **kwargs)
-	#
-        #  Data
-        #
-
-        #
-	#  Window Objects.
-	#
-        self.__make_objects()
-	#
-	#  Bindings.
-	#
-	self.__make_bindings()
-	#
-	#  Sizing.
-	#
-	self.__make_sizers()
-	#
-	self.SetAutoLayout(True)
-        self.SetSizerAndFit(self.sizer)
-	#
-	return
-    #
-    # ============================== Internal Methods
-    #
-    def __make_objects(self):
-        """Add interactors"""
-
-        self.tbarSizer = makeTitleBar(self, 'Grain List',
-                                      color=WP.BG_COLOR_PANEL1_TITLEBAR)
-        self.listctrl = self.__make_listctrl()
-        self.ginfo_but = wx.Button(self, wx.NewId(), 'show info')
-
-        return
-    
-    def __make_listctrl(self):
-	"""Make the list control"""
-        #
-	LStyle = wx.LC_REPORT
-	#
-	listctrl = wx.ListView(self, wx.NewId(), style=LStyle)
-	listctrl.InsertColumn(0, ' ID ')
-	listctrl.InsertColumn(1, 'completeness')
-        listctrl.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)
-        listctrl.SetColumnWidth(1, wx.LIST_AUTOSIZE_USEHEADER)
-
-        return listctrl
-
-    def __make_bindings(self):
-        """Bind interactors"""
-        self.Bind(wx.EVT_BUTTON, self.OnTable, self.ginfo_but)
-        
-        return
-
-    def __make_sizers(self):
-	"""Lay out the interactors"""
-	
-	self.sizer = wx.BoxSizer(wx.VERTICAL)
-	self.sizer.Add(self.tbarSizer, 0, wx.EXPAND|wx.ALIGN_CENTER)
-        self.sizer.Add(self.listctrl)
-        self.sizer.Add(self.ginfo_but, 0, wx.ALIGN_CENTER)
-
-	return
-    #
-    # ============================== API
-    #
-    #                     ========== *** Access Methods
-    #
-
-    #
-    #                     ========== *** Event Callbacks
-    #
-    def OnTable(self, e):
-        """Raise table with grain info"""
-        dlg = GrainTableDlg(self, wx.NewId())
-        dlg.ShowModal()
-        
-        return
-    
-    pass # end class
-#
-# -----------------------------------------------END CLASS:  GrainListSubPanel
-# ---------------------------------------------------CLASS:  RefinementSubPanel
-#
-class RefinementSubPanel(wx.Panel):
-    """RefinementSubPanel """
-    def __init__(self, parent, id, **kwargs):
-	"""Constructor for RefinementSubPanel."""
-	#
-	wx.Panel.__init__(self, parent, id, **kwargs)
-	#
-        #  Data
-        #
-
-        #
-	#  Window Objects.
-	#
-        self.__make_objects()
-	#
-	#  Bindings.
-	#
-	self.__make_bindings()
-	#
-	#  Sizing.
-	#
-	self.__make_sizers()
-	#
-	self.SetAutoLayout(True)
-        self.SetSizerAndFit(self.sizer)
-	#
-	return
-    #
-    # ============================== Internal Methods
-    #
-    def __make_objects(self):
-        """Add interactors"""
-
-        self.tbarSizer = makeTitleBar(self, 'Grain Refinement',
-                                        color=WP.BG_COLOR_PANEL1_TITLEBAR)
-        self.refine_but  = wx.Button(self, wx.NewId(), 'refine')
-
-        return
-
-    def __make_bindings(self):
-        """Bind interactors"""
-        return
-
-    def __make_sizers(self):
-	"""Lay out the interactors"""
-	
-	self.sizer = wx.BoxSizer(wx.VERTICAL)
-	self.sizer.Add(self.tbarSizer, 0, wx.EXPAND|wx.ALIGN_CENTER)
-        self.sizer.Add(self.refine_but, 0, wx.ALIGN_CENTER|wx.TOP, 5)
-
-	return
-    #
-    # ============================== API
-    #
-    #                     ========== *** Access Methods
-    #
-
-    #
-    #                     ========== *** Event Callbacks
-    #
-    
-    pass # end class
-#
-# -----------------------------------------------END CLASS:  RefinementSubPanel
-# ---------------------------------------------------CLASS:  GrainTablePanel
-#
-class GrainTablePanel(wx.Panel):
-    """GrainTablePanel """
-    def __init__(self, parent, id, **kwargs):
-	"""Constructor for GrainTablePanel."""
-	#
-	wx.Panel.__init__(self, parent, id, **kwargs)
-	#
-        #  Data
-        #
-
-        #
-	#  Window Objects.
-	#
-        self.__make_objects()
-	#
-	#  Bindings.
-	#
-	self.__make_bindings()
-	#
-	#  Sizing.
-	#
-	self.__make_sizers()
-	#
-	self.SetAutoLayout(True)
-        self.SetSizerAndFit(self.sizer)
-	#
-	return
-    #
-    # ============================== Internal Methods
-    #
-    def __make_objects(self):
-        """Add interactors"""
-
-        self.tbarSizer = makeTitleBar(self, 'Grain Table')
-        self.listctrl = self.__make_listctrl()
-        return
-
-    def __make_listctrl(self):
-	"""Make the list control"""
-        #
-	LStyle = wx.LC_REPORT
-	#
-	listctrl = wx.ListView(self, wx.NewId(), style=LStyle)
-	listctrl.InsertColumn(0, 'spot ID')
-	listctrl.InsertColumn(1, '  hkl  ')
-        listctrl.SetColumnWidth(0, wx.LIST_AUTOSIZE_USEHEADER)
-        listctrl.SetColumnWidth(1, wx.LIST_AUTOSIZE_USEHEADER)
-
-        return listctrl
-
-    def __make_bindings(self):
-        """Bind interactors"""
-        return
-
-    def __make_sizers(self):
-	"""Lay out the interactors"""
-	
-	self.sizer = wx.BoxSizer(wx.VERTICAL)
-	self.sizer.Add(self.tbarSizer, 0, wx.EXPAND|wx.ALIGN_CENTER)
-	self.sizer.Add(self.listctrl, 1, wx.EXPAND|wx.ALIGN_CENTER)
-
-	return
-    #
-    # ============================== API
-    #
-    #                     ========== *** Access Methods
-    #
-
-    #
-    #                     ========== *** Event Callbacks
-    #
-    
-    pass # end class
-#
-# -----------------------------------------------END CLASS:  GrainTablePanel
-# ---------------------------------------------------CLASS:  GrainTableDlg
-#
-class GrainTableDlg(wx.Dialog):
-    """GrainTableDlg """
-    def __init__(self, parent, id, **kwargs):
-	"""Constructor for GrainTableDlg"""
-	#
-        myStyle = wx.RESIZE_BORDER|wx.DEFAULT_DIALOG_STYLE
-	wx.Dialog.__init__(self, parent, id, style=myStyle)
-	#
-	#  Data Objects.
-	#
-	
-	#
-	#  Windows.
-	#
-	self.titlebar = wx.StaticText(self, -1, 'Grain Table Dialog', 
-				      style=wx.ALIGN_CENTER|wx.SIMPLE_BORDER)
-        self.panel = GrainTablePanel(self, wx.NewId())
-	#
-	#  Bindings.
-	#
-	self._makeBindings()
-	#
-	#  Sizing.
-	#
-	self._makeSizers()
-	#
-	self.SetAutoLayout(True)
-        self.SetSizerAndFit(self.sizer)
-	#
-	#  Events.
-	#
-	
-	#
-	return
-    #
-    # ============================== Internal Methods
-    #
-    def _makeBindings(self):
-	"""Bind interactors to functions"""
-	return
-
-    def _makeSizers(self):
-	"""Lay out windows"""
-	self.sizer = wx.BoxSizer(wx.VERTICAL)
-	self.sizer.Add(self.titlebar, 0, wx.EXPAND|wx.ALIGN_CENTER)
-	self.sizer.Add(self.panel, 1, wx.EXPAND|wx.ALIGN_CENTER)
-
-	return
-    #
-    # ============================== API
-    #
-    #                     ========== *** Access Methods
-    #
-
-    #
-    #                     ========== *** Event Callbacks
-    #
-    
 
     pass # end class
 #
-# -----------------------------------------------END CLASS:  GrainTableDlg
-#
+# -----------------------------------------------END CLASS:  grainPanel
