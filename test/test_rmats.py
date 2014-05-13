fdb867d6ad8e44293f0f0bfb418a3abe946ec574
Joel Bernier <bernier2@llnl.gov>
2014-05-13 13:36:50 -0700

    Added test of rotation conversion functions in CAPI

---
+++ test/test_rmats.py
@@ -0,0 +1,29 @@
+import sys, os, time
+
+import numpy as np
+
+from hexrd import matrixutil as mutil
+
+from hexrd.xrd import rotations as rot
+from hexrd.xrd import transforms_CAPI as xfcapi
+
+n_quats = 1000000
+rq = mutil.unitVector(np.random.randn(4, n_quats))
+quats = np.array(rq.T, dtype=float, order='C')
+
+# phi = 2. * rot.arccosSafe(rq[0, :])
+# n   = np.tile(1. / np.sin(0.5*phi), (3, 1)) * rq[1:, :]
+
+start0 = time.clock()                      # time this
+rMats0 = rot.rotMatOfQuat(rq)
+elapsed0 = (time.clock() - start0)
+
+# rMats1 = np.zeros((n_quats, 3, 3))
+# for i in range(n_quats):
+#     rMats1[i, :, :] = xfcapi.makeRotMatOfQuat(quats[i, :])
+
+start1 = time.clock()                      # time this
+rMats1 = xfcapi.makeRotMatOfQuat(quats)
+elapsed1 = (time.clock() - start1)
+print "Time for %d quats:\t%g v. %g (%f)"%(n_quats, elapsed0/float(n_quats), elapsed1/float(n_quats), elapsed0/elapsed1)
+print "Maximum discrepancy:\t%f" % (np.amax(abs(rMats0 - rMats1)))
