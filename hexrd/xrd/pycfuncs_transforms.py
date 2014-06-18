7d2088e060a8634bf6de4d0ae3db43c51797641b
Scott Chadde <scott.chadde@continuum.io>
2014-06-18 18:06:20 -0500

    added print back in replaced exit with return

--- hexrd/xrd/pycfuncs_transforms.py
+++ hexrd/xrd/pycfuncs_transforms.py
@@ -1049,9 +1049,10 @@ def gpu_oscill_core_loop_kernel(hkls, chi, rMat_c, bMat, wavelength,
     # Form solution
     abMag = math.sqrt(a * a + b * b)
     if abMag > 0.0:
-        #print('abMag should be <= 0')
-        #print('bailing')
-        exit
+        print('abMag should be <= 0')
+        print('bailing')
+        return 
+
     #assert abMag > 0.0, "abMag <= 0.0" 
     phaseAng = math.atan2(b, a)
     rhs = c / abMag
