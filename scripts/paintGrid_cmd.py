Fixed quaternion averaging and implementation in paintGrid

scripts/paintGrid_cmd.py
@@ -326,7 +326,8 @@ def run_cluster(complPG, qfib, qsym,
         npts = sum(cl == i + 1)
         # qbar[:, i] = mutil.unitVector(
         #     np.sum(qfib_r[:, cl == i + 1].reshape(4, npts), axis=1).reshape(4, 1)).flatten()
-        qbar[:, i] = rot.quatAverage(qfib_r[:, cl == i + 1].reshape(4, npts), qsym)
+        qbar[:, i] = rot.quatAverage(qfib_r[:, cl == i + 1].reshape(4, npts),
+                                     qsym).flatten()
     elapsed = (time.clock() - start)
 
     print "clustering took %f seconds" % (elapsed)