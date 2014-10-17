495f4af470b2df9163a147117b0720b2efc4ac69
Darren Dale <dsdale24@gmail.com>
2014-10-16 22:34:25 -0400

    renames paintgrid script and installs it

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ for dirpath, dirnames, filenames in os.walk('hexrd'):
     else:
         del(dirnames[:])
 
-scripts = []
+scripts = ['paintGrid_cmd.py']
 if sys.platform.startswith('win'):
     # scripts calling multiprocessing must be importable
     import shutil
