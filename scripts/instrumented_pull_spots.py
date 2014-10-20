cd3da4c190abf591b938c4bd7c6dfa1ba92e8428
Oscar Villellas <oscar.villellas@continuum.io>
2014-10-20 16:14:30 +0200

    added nvtx instrumentation to the instrumented script

--- scripts/instrumented_pull_spots.py
+++ scripts/instrumented_pull_spots.py
@@ -6,27 +6,93 @@
 import pull_spots_mp as target
 
 import sys, os, time
-import cProfile as profile
 import functools
 from contextlib import contextmanager
-
-
 import numpy as np
 
+def add_nvtx_instrumentation(nvtx):
+    from scipy import sparse
+    from scipy import ndimage
+    from hexrd.xrd import xrdutil
+    from hexrd.xrd import fitting
+    from hexrd.xrd import xrdutil
+    from hexrd.xrd import distortion
+    from hexrd import gridutil
+    from numba import dispatcher
+
+    _locals = locals()
+    def PROFILE(func_path, color):
+        exec '{0} = nvtx.profiled("{0}", color={1})({0})'.format(func_path, color) in globals(), _locals
+
+
+    # This 3 show a bit of structure:
+    # target.Config.__init__ is initialization time that is outside of multiprocessing
+    # target.read_frames is the time taken by reading frame data *and* converting to sparse
+    # target.process_grain is the "per grain" process, which should be handled by multiproc
+    PROFILE('target.Config.__init__', nvtx.colors.red)
+    PROFILE('target.read_frames', nvtx.colors.green)
+    PROFILE('target.process_grain', nvtx.colors.blue)
+
+    # This shows where numba is compiling the @jit decorated functions
+    PROFILE('dispatcher.Overloaded.compile', nvtx.colors.black)
+
+    # some key functions
+    PROFILE('xrdutil.pullSpots', nvtx.colors.blue)
+    PROFILE('fitting.fitGrain', nvtx.colors.yellow)
+    PROFILE('fitting.objFuncFitGrain', nvtx.colors.magenta)
+    PROFILE('sparse.coo_matrix', nvtx.colors.yellow)
+    PROFILE('sparse.coo.coo_matrix.todense', nvtx.colors.magenta)
+    PROFILE('gridutil.computeArea', nvtx.colors.green)
+    PROFILE('gridutil.sutherlandHodgman', nvtx.colors.blue)
+    PROFILE('xrdutil.simulateGVecs', nvtx.colors.yellow)
+    PROFILE('gridutil.cellCentroids', nvtx.colors.cyan)
+    PROFILE('gridutil.cellConnectivity', nvtx.colors.red)
+    PROFILE('ndimage.label', nvtx.colors.white)
+    PROFILE('distortion._ge_41rt_distortion', nvtx.colors.yellow)
+    PROFILE('distortion._ge_41rt_inverse_distortion', nvtx.colors.yellow)
+
+
+def print_nvtx_profile(nvtx):
+    print " STATS ".center(72, '=')
+    fmt = "{2:>14}, {1:>8}, {0:<40}"
+    print fmt.format("FUNCTION", "CALLS", "TIME")
+    fmt = "{2:>14F}, {1:>8}, {0:<40}"
+    sorted_by_time = sorted(nvtx.getstats().iteritems(), key=lambda tup: tup[1][1])
+    for key, val in sorted_by_time:
+        print fmt.format(key, *val)
+
 @contextmanager
-def profiling(pr_file=None, use_nvtx=False):
-    pr = profile.Profile()
-    pr.enable()
+def profiling(profile=False, use_nvtx=False):
+    if use_nvtx:
+        try:
+            from numbapro import nvtx
+            add_nvtx_instrumentation(nvtx)
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            print "Could not import nvtx, skipping nvtx profile"
+            use_nvtx = False
+
+    if profile:
+        import cProfile as profile
+        pr = profile.Profile()
+        pr.enable()
+
     yield
-    pr.disable()
-    pr.print_stats(sort='cumulative')
+
+    if profile:
+        pr.disable()
+        pr.print_stats(sort='cumulative')
+
+    if use_nvtx:
+        print_nvtx_profile(nvtx)
     
 
 if __name__ == '__main__':
     cfg_filename = sys.argv[1]
     quats_filename = sys.argv[2] if len(sys.argv) >= 3 else None
 
-    with profiling():
+    with profiling(use_nvtx=True):
         start = time.time()
         print "Using cfg file '%s'" % (cfg_filename)
         config = target.Config(cfg_filename)
