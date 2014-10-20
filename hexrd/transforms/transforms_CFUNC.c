4edaad14c2ba07a517eda3e3ac8506b7532e8f6f
Mark Wiebe <mwiebe@continuum.io>
2014-10-20 15:44:57 -0500

    Add boilerplate for array version of gvecToDetectorXY
    and split out the single transformation into a separate function

--- hexrd/transforms/transforms_CFUNC.c
+++ hexrd/transforms/transforms_CFUNC.c
@@ -14,6 +14,69 @@ static double Zl[3] = {0.0,0.0,1.0};
 /******************************************************************************/
 /* Funtions */
 
+void gvecToDetectorXYOne_cfunc(double * gVec_c, double * rMat_d,
+                               double * rMat_sc, double * tVec_d,
+                               double * bHat_l,
+                               double * nVec_l, double num, double * P0_l,
+                               double * result)
+{
+  int j, k;
+  double bDot, ztol, denom, u;
+  double gHat_c[3], gVec_l[3], dVec_l[3], P2_l[3], P2_d[3];
+  double brMat[9];
+
+  ztol = epsf;
+
+  /* Compute unit reciprocal lattice vector in crystal frame w/o translation */
+  unitRowVector_cfunc(3,gVec_c,gHat_c);
+
+  /* Compute unit reciprocal lattice vector in lab frame and dot with beam vector */
+  bDot = 0.0;
+  for (j=0; j<3; j++) {
+    gVec_l[j] = 0.0;
+    for (k=0; k<3; k++)
+      gVec_l[j] += rMat_sc[3*j+k]*gHat_c[k];
+
+    bDot -= bHat_l[j]*gVec_l[j];
+  }
+
+  if ( bDot >= ztol && bDot <= 1.0-ztol ) {
+    /* If we are here diffraction is possible so increment the number of admissable vectors */
+    makeBinaryRotMat_cfunc(gVec_l,brMat);
+
+    denom = 0.0;
+    for (j=0; j<3; j++) {
+      dVec_l[j] = 0.0;
+      for (k=0; k<3; k++)
+        dVec_l[j] -= brMat[3*j+k]*bHat_l[k];
+
+      denom += nVec_l[j]*dVec_l[j];
+    }
+
+    if ( denom < -ztol ) {
+
+      u = num/denom;
+
+      for (j=0; j<3; j++)
+        P2_l[j] = P0_l[j]+u*dVec_l[j];
+
+      for (j=0; j<2; j++) {
+        P2_d[j] = 0.0;
+        for (k=0; k<3; k++)
+          P2_d[j] += rMat_d[3*k+j]*(P2_l[k]-tVec_d[k]);
+        result[j] = P2_d[j];
+      }
+    } else {
+      result[0] = NAN;
+      result[1] = NAN;
+    }
+
+  } else {
+    result[0] = NAN;
+    result[1] = NAN;
+  }
+}
+
 void gvecToDetectorXY_cfunc(long int npts, double * gVec_c,
                             double * rMat_d, double * rMat_s, double * rMat_c,
                             double * tVec_d, double * tVec_s, double * tVec_c,
@@ -22,11 +85,9 @@ void gvecToDetectorXY_cfunc(long int npts, double * gVec_c,
   long int i;
   int j, k, l;
 
-  double ztol, bDot, denom, u, num;
-  double nVec_l[3], bHat_l[3], P0_l[3], P2_l[3], P2_d[3], P3_l[3], gHat_c[3], gVec_l[3], dVec_l[3];
-  double rMat_sc[9], brMat[9];
-
-  ztol = epsf;
+  double num;
+  double nVec_l[3], bHat_l[3], P0_l[3], P3_l[3];
+  double rMat_sc[9];
 
   /* Normalize the beam vector */
   unitRowVector_cfunc(3,beamVec,bHat_l);
@@ -58,57 +119,18 @@ void gvecToDetectorXY_cfunc(long int npts, double * gVec_c,
   }
 
   for (i=0L; i<npts; i++) {
-
-    /* Compute unit reciprocal lattice vector in crystal frame w/o translation */
-    unitRowVector_cfunc(3,&gVec_c[3*i],gHat_c);
-
-    /* Compute unit reciprocal lattice vector in lab frame and dot with beam vector */
-    bDot = 0.0;
-    for (j=0; j<3; j++) {
-      gVec_l[j] = 0.0;
-      for (k=0; k<3; k++)
-        gVec_l[j] += rMat_sc[3*j+k]*gHat_c[k];
-
-      bDot -= bHat_l[j]*gVec_l[j];
-    }
-
-    if ( bDot >= ztol && bDot <= 1.0-ztol ) {
-      /* If we are here diffraction is possible so increment the number of admissable vectors */
-      makeBinaryRotMat_cfunc(gVec_l,brMat);
-
-      denom = 0.0;
-      for (j=0; j<3; j++) {
-        dVec_l[j] = 0.0;
-        for (k=0; k<3; k++)
-          dVec_l[j] -= brMat[3*j+k]*bHat_l[k];
-
-        denom += nVec_l[j]*dVec_l[j];
-      }
-
-      if ( denom < -ztol ) {
-
-        u = num/denom;
-
-        for (j=0; j<3; j++)
-          P2_l[j] = P0_l[j]+u*dVec_l[j];
-
-        for (j=0; j<2; j++) {
-          P2_d[j] = 0.0;
-          for (k=0; k<3; k++)
-            P2_d[j] += rMat_d[3*k+j]*(P2_l[k]-tVec_d[k]);
-          result[2*i+j] = P2_d[j];
-        }
-      } else {
-        result[2*i+0] = NAN;
-        result[2*i+1] = NAN;
-      }
-
-    } else {
-      result[2*i+0] = NAN;
-      result[2*i+1] = NAN;
-    }
+    gvecToDetectorXYOne_cfunc(&gVec_c[3*i], rMat_d, rMat_sc, tVec_d,
+                              bHat_l, nVec_l, num,
+                              P0_l, &result[2*i]);
   }
+}
 
+void gvecToDetectorXYArray_cfunc(long int npts, double * gVec_c,
+                            double * rMat_d, double * rMat_s, double * rMat_c,
+                            double * tVec_d, double * tVec_s, double * tVec_c,
+                            double * beamVec, double * result)
+{
+  // TODO
 }
 
 void detectorXYToGvec_cfunc(long int npts, double * xy,
