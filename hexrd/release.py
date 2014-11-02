f5ff2b3709c7e05ef5f30a5d8f28a4201ca5d64b
Darren Dale <dsdale24@gmail.com>
2014-11-02 12:20:07 -0500

    bumps version to 0.2.0.dev

--- hexrd/release.py
+++ hexrd/release.py
@@ -19,10 +19,10 @@ name = 'hexrd'
 # release.  'dev' as a _version_extra string means this is a development
 # version
 _version_major = 0
-_version_minor = 1
-_version_patch = 1
-_version_extra = 'dev'
-# _version_extra = 'rc1'
+_version_minor = 2
+_version_patch = 0
+_version_extra = '.dev' # uncomment this during development
+# _version_extra = 'rc1' # uncomment this for release candidates
 # _version_extra = ''  # Uncomment this for full releases
 
 # Construct full version string from these.
@@ -30,7 +30,7 @@ _ver = [_version_major, _version_minor, _version_patch]
 
 __version__ = '.'.join(map(str, _ver))
 if _version_extra:
-    __version__ = __version__ + '-' + _version_extra
+    __version__ = __version__ + _version_extra
 
 version_info = (_version_major, _version_minor, _version_patch, _version_extra)
 
