d35c6245d773057d9c7a9e0d1b2ed8e7bfce9572
Darren Dale <dsdale24@gmail.com>
2014-11-07 09:47:12 -0500

    version info for 0.2.1

--- hexrd/release.py
+++ hexrd/release.py
@@ -22,7 +22,7 @@ _version_major = 0
 _version_minor = 2
 _version_patch = 1
 _version_extra = ''
-_version_extra = '.dev' # uncomment this during development
+#_version_extra = '.dev' # uncomment this during development
 # _version_extra = 'rc1' # uncomment this for release candidates
 
 # Construct full version string from these.
