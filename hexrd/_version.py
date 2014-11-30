81c6cc86bda6fb964f88eee5b02ce62444f56f78
Darren Dale <dsdale24@gmail.com>
2014-11-30 09:38:35 -0500

    fixes a versioneer bug on windows

--- hexrd/_version.py
+++ hexrd/_version.py
@@ -169,11 +169,11 @@ def get_versions(default={"version": "unknown", "full": ""}, verbose=False):
         return ver
 
     try:
-        root = os.path.abspath(__file__)
+        root = __file__
         # versionfile_source is the relative path from the top of the source
         # tree (where the .git directory might live) to this file. Invert
         # this to find the root from __file__.
-        for i in range(len(versionfile_source.split(os.sep))):
+        for i in versionfile_source.split('/'):
             root = os.path.dirname(root)
     except NameError:
         return default
