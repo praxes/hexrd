bb57ea77df343545e41a0f6987b0fd473bf76ff1
Oscar Villellas <oscar.villellas@continuum.io>
2015-03-17 16:44:17 +0100

    add support for instrumented profile in herd

--- hexrd/cli/main.py
+++ hexrd/cli/main.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import sys
 import multiprocessing
+import warnings
 
 # These can't be relative imports on Windows because of the hack
 # in main() for multiprocessing.freeze_support()
@@ -14,6 +15,101 @@ from hexrd.cli import fitgrains
 from hexrd.cli import gui
 from hexrd.cli import help
 from hexrd.cli import test
+
+try:
+    from numbapro import nvtx
+except ImportError:
+    pass
+
+try:
+    import importlib
+except ImportError:
+    pass
+
+
+def profile_instrument_function(fn_desc):
+    """fn_desc contains:
+    'fn' is the full path to the function
+    """
+
+    # we must split 'fn' into the module and the function itself. Note
+    # this is not trivial as we may find several levels of objects inside
+    # the containing module. Try sequentially...
+    full_name = fn_desc['fn']
+    color = fn_desc.get('color', 'black')
+    color = getattr(nvtx.colors, color, nvtx.colors.black)
+    parts = full_name.split('.')
+
+    # last item will always be the function name
+    fn_name = parts[-1]
+
+    # number of parts in the path
+    path_parts = len(parts) - 1
+
+    # consume as many as possible with import (ignore last part that is the function name)
+    pos = 0
+    for i in range(1, path_parts+1):
+        try:
+            m = importlib.import_module('.'.join(parts[0:i]))
+            pos = i
+        except ImportError as e:
+            break
+
+    # at this point, i points at the starting of the dotted path to the function
+    # to instrument... follow the parts till we get to the actual function
+    try:
+        o = m
+        for i in range(pos, path_parts):
+            o = getattr(o, parts[i])
+
+        # instrument...
+        original = getattr(o, fn_name)
+        override = nvtx.profiled(full_name, color=color)(original)
+        setattr(o, fn_name, override)
+    except AttributeError:
+        raise
+        warnings.warn('Could not instrument "{0}"'.format(full_name))
+
+
+
+def profile_parse_file(filename):
+    try:
+        import yaml
+        with open(filename, 'r') as f:
+            cfg = yaml.load(f)
+
+        if 'profile' not in cfg:
+            warnings.warn('profile file "{0}" missing a profile section'.format(filename))
+            return
+
+        profile_cfg = cfg['profile']
+        if 'instrument' in profile_cfg:
+            # instrument all
+            [profile_instrument_function(fn_desc) for fn_desc in profile_cfg['instrument']]
+
+
+    except Exception as e:
+        raise
+        warnings.warn('Failed to include profile file: {0}'.format(filename))
+        warnings.warn(str(e))
+
+
+def profile_instrument_all(args):
+    """
+    args contains a list of the yaml files containing instrumentation
+    information
+    """
+    [profile_parse_file(filename) for filename in args]
+
+
+def profile_dump_results(args):
+    print(" STATS ".center(72, '='))
+    fmt = "{2:>14}, {1:>8}, {0:<40}"
+    print(fmt.format("FUNCTION", "CALLS", "TIME"))
+    fmt = "{2:>14F}, {1:>8}, {0:<40}"
+    sorted_by_time = sorted(nvtx.getstats().iteritems(), key=lambda tup: tup[1][1])
+    for key, val in sorted_by_time:
+        print(fmt.format(key, *val))
 
 
 def main():
@@ -27,7 +123,6 @@ def main():
     if len(sys.argv) == 1:
         sys.argv.append('-h')
 
-    import logging
     import hexrd
 
     p = argparse.ArgumentParser(
@@ -42,6 +137,11 @@ def main():
         "--debug",
         action = "store_true",
         help = 'verbose reporting',
+    )
+    p.add_argument(
+        "--inst-profile",
+        action="append",
+        help='use the following files as source for functions to instrument',
     )
     sub_parsers = p.add_subparsers(
         metavar = 'command',
@@ -69,4 +169,10 @@ def main():
     ch = logging.StreamHandler()
     ch.setLevel(log_level)
 
+    if hasattr(args, 'inst_profile'):
+        profile_instrument_all(args.inst_profile)
+
     args.func(args, p)
+
+    if hasattr(args, 'inst_profile'):
+        profile_dump_results(args.inst_profile)
