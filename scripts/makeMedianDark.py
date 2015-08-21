1e75f21375bfe9048850dbd1e358823544199449
joelvbernier <joelvbernier@me.com>
2015-08-21 09:37:20 -0700

    add functions and make script cfg compatible

--- scripts/makeMedianDark.py
+++ scripts/makeMedianDark.py
@@ -1,61 +1,74 @@
+#!/usr/bin/env python
 import sys, os
 
-from hexrd.xrd import detector
-from hexrd.xrd import xrdutil
+import numpy as np
 
-fileInfo = [('/home/bernier2/Documents/data/ruby_00025.ge3', 2), 
-            ('/home/bernier2/Documents/data/ruby_00026.ge3', 2)]
+from hexrd import config
+from hexrd.fitgrains import get_instrument_parameters
 
-nChunk      = 5
-medianSize  = 3
-medianRange = (-50,50)
-
-def medianDarkGE(fileInfo, nChunk, medianSize, medianRange):
-    reader    = detector.ReadGE(fileInfo, subtractDark=False, doFlip=False)    
-    numFrames = reader.getNFrames()
-    mdresults = xrdutil.darkFromStack(reader, nFrames=numFrames, nChunk=nChunk, 
-                                      medianSize=medianSize, medianRange=medianRange, 
-                                      checkIntensityResolution=False)
-    darkFrame, deadPixels, std = mdresults
-    return darkFrame, deadPixels, std
-
-if __name__ == "__main__":
+def make_dark_frame(cfg, nframes_use=100, nbytes_header=8192, pixel_type=np.uint16):
     """
-    Usage:
+    FOR BYTE STREAM IMAGES ONLY!
 
-    python makeMedianDark.py LT3MEW ge2 341 341 5 1 ./ dark
+    nbytes_header and pixel_bytes default for raw GE
     """
-    argv = sys.argv[1:]
     
-    fileRoot   = argv[0]
-    fileSuffix = argv[1]
-    fileStart  = argv[2]
-    fileStop   = argv[3]
-    zpad       = argv[4]
-    nEmtpy     = argv[5]
-    outputDir  = argv[6]
-    outputName = argv[7]
+    nempty = cfg.image_series.images.start
+    image_number = cfg.image_series.file.ids[0]
+
+    instr_cfg = get_instrument_parameters(cfg)
+
+    nrows = instr_cfg['detector']['pixels']['rows']
+    ncols = instr_cfg['detector']['pixels']['columns']
+
+    pixel_bytes = pixel_type().itemsize
+    nbytes_frame = pixel_bytes*nrows*ncols    # uint16
+
+    fid = open(cfg.image_series.file.stem %cfg.image_series.file.ids[0], 'rb')
+    fid.seek(nbytes_header+nempty*nbytes_frame, 0)     # header plus junk frames
+    frames = np.frombuffer(fid.read(nframes_use*nbytes_frame), dtype=pixel_type).reshape(nframes_use, nrows, ncols)
+    fid.close()
     
-    numImages = (int(fileStop) - int(fileStart)) + 1
-    fileInfo = []
-    zpad_str = "_%"+"0%dd" % (int(zpad))
+    dark = np.median(frames, axis=0)
     
-    for i in range(numImages):
-        if fileSuffix.strip() == '':
-            fname = fileRoot+zpad_str
-            oname = outputName
-        else:
-            fname = fileRoot+zpad_str+"."+fileSuffix 
-            oname = outputName+'.'+fileSuffix
-        fileInfo.append( ( fname % (int(fileStart) + i), int(nEmtpy) ) )
-        pass
+    # usi UNIX style path spitting...
+    image_stem = cfg.image_series.file.stem.split('/')[-1].split('.')
+    if len(image_stem) > 1:
+        suffix = '.'+image_stem[-1]
+    else:
+        suffix = ''
+    output_name = os.path.join(cfg.working_dir, 
+                               image_stem[0] %image_number + '_medianDark' + suffix)
+
+    fid = open(output_name, 'wb')
+    fid.seek(nbytes_header)
+    fid.write(dark.astype(np.uint16))
+    fid.close()
+    return dark.astype(np.uint16)
     
-    print "processing fileInfo: "
-    print fileInfo
+def subtract_dark_and_write(cfg, dark, filename='full_scan.ge'):
+    archive = open(os.path.join(cfg.working_dir, filename), 'wb')
+    archive.seek(nbytes_header)
+    for f in cfg.image_series.files:
+        fid = open(f, 'rb')
+        fid.seek(nbytes_header, 0)     # header plus first junk frame
+        for iframe in range(int(cfg.image_series.n_frames)):
+            frame = np.frombuffer(fid.read(nbytes_frame), dtype=np.uint16).reshape(nrows, ncols)
+            tmp = frame - dark
+            tmp[tmp <= cfg.find_orientations.orientation_maps.threshold] = 0
+            archive.write(tmp)
+        fid.close()
+    archive.close()
+    return
     
-    darkFrame, deadPixels, std = medianDarkGE(fileInfo, nChunk, medianSize, medianRange)
-    fw = detector.FrameWriter(2048, 2048, 
-                              filename=os.path.join(outputDir, oname), 
-                              nbytesHeader=8192)
-    fw.write(darkFrame)
-    fw.close()
+if __name__ == '__main__':
+    config_filename = sys.argv[1]
+    cfg = config.open(config_filename)[0]
+    
+    if len(sys.argv) > 2:
+        dark = make_dark_frame(cfg, nframes_use=int(sys.argv[2]))
+    else:
+        dark = make_dark_frame(cfg)
+        #subtract_dark_and_write(cfg, dark)
+    
+    
