d0bea6ec0e6b9c69351c2d51afb13ce7d56701ed
Donald Boyce <donald.e.boyce@gmail.com>
2015-09-13 16:03:04 -0400

    added array adapter for imageseries

---
+++ hexrd/imageseries/adapters/array.py
@@ -0,0 +1,47 @@
+"""Adapter class for numpy array (3D)
+"""
+from . import ImageSeriesAdapter
+from ..imageseriesiter import ImageSeriesIterator
+
+class ArrayImageSeriesAdapter(ImageSeriesAdapter):
+    """collection of images in numpy array"""
+
+    format = 'array'
+
+    def __init__(self, fname, **kwargs):
+        """Constructor for frame cache image series
+
+        *fname* - should be None 
+        *kwargs* - keyword arguments
+                 . 'data' = a 3D array (double/float)
+                 . 'metadata' = a dictionary
+        """
+        self._data = kwargs['data']
+        self._meta = kwargs.pop('metadata', dict())
+        self._shape = self._data.shape
+        self._nframes = self._shape[0]
+        self._nxny = self._shape[1:3]
+
+    @property
+    def metadata(self):
+        """(read-only) Image sequence metadata
+
+        Currently returns none
+        """
+        return self._meta
+
+    @property
+    def shape(self):
+        return self._nxny
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __iter__(self):
+        return ImageSeriesIterator(self)
+
+    #@memoize
+    def __len__(self):
+        return self._nframes
+
+    pass  # end class
