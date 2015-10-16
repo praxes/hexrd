5a88d88fa71ae3fa1f9789380d3bed49854beb6d
joelvbernier <joelvbernier@me.com>
2015-10-16 10:44:03 -0700

    added neighborhood size estimation to findorientations

--- hexrd/findorientations.py
+++ hexrd/findorientations.py
@@ -48,14 +48,13 @@ except ImportError:
 
 
 def generate_orientation_fibers(eta_ome, threshold, seed_hkl_ids, fiber_ndiv):
-    """ From ome-eta maps and hklid spec, generate list of
+    """ 
+    From ome-eta maps and hklid spec, generate list of
     quaternions from fibers
-
-    ** passing of BOTH pd and eta_ome object is redundant; flag for fix!
     """
     # seed_hkl_ids must be consistent with this...
     pd_hkl_ids = eta_ome.iHKLList[seed_hkl_ids]
-
+    
     # grab angular grid infor from maps
     del_ome = eta_ome.omegas[1] - eta_ome.omegas[0]
     del_eta = eta_ome.etas[1] - eta_ome.etas[0]
@@ -69,7 +68,7 @@ def generate_orientation_fibers(eta_ome, threshold, seed_hkl_ids, fiber_ndiv):
     bMat = pd.latVecOps['B']
     csym = pd.getLaueGroup()
     qsym = pd.getQSym()
-
+    
     ############################################
     ##    Labeling of spots from seed hkls    ##
     ############################################
@@ -132,7 +131,7 @@ def generate_orientation_fibers(eta_ome, threshold, seed_hkl_ids, fiber_ndiv):
     return np.hstack(qfib)
 
 
-def run_cluster(compl, qfib, qsym, cfg):
+def run_cluster(compl, qfib, qsym, cfg, min_samples=None):
     """
     """
     cl_radius = cfg.find_orientations.clustering.radius
@@ -274,25 +273,56 @@ def generate_eta_ome_maps(cfg, pd, reader, detector, hkls=None):
 
 
 def find_orientations(cfg, hkls=None, profile=False):
-    """Takes a config dict as input, generally a yml document"""
+    """
+    Takes a config dict as input, generally a yml document
+    
+    NOTE: single cfg instance, not iterator!
+    """
 
     # a goofy call, could be replaced with two more targeted calls
     pd, reader, detector = initialize_experiment(cfg)
 
+    # need instrument cfg later on down...
+    instr_cfg = get_instrument_parameters(cfg)
+    detector_parameters = np.hstack([
+        instr_cfg['detector']['transform']['tilt_angles'],
+        instr_cfg['detector']['transform']['t_vec_d'],
+        instr_cfg['oscillation_stage']['chi'],
+        instr_cfg['oscillation_stage']['t_vec_s'],
+        ])
+    rdim = cfg.instrument.detector.pixels.size[0]*cfg.instrument.detector.pixels.rows
+    cdim = cfg.instrument.detector.pixels.size[1]*cfg.instrument.detector.pixels.columns
+    panel_dims = ((-0.5*cdim, -0.5*rdim),
+                  ( 0.5*cdim,  0.5*rdim),
+                  )
+    # UGH! hard-coded distortion...
+    if instr_cfg['distortion']['function_name'] == 'GE_41RT':
+        distortion = (dFuncs.GE_41RT,
+                      instr_cfg['distortion']['parameters'],
+                      )
+    else:
+        distortion = None
+
+    # start logger
     logger.info("beginning analysis '%s'", cfg.analysis_name)
 
     # load the eta_ome orientation maps
     eta_ome = load_eta_ome_maps(cfg, pd, reader, detector, hkls)
 
+    ome_range = (np.min(eta_ome.omeEdges),
+                 np.max(eta_ome.omeEdges)
+                 )
     try:
         # are we searching the full grid of orientation space?
         qgrid_f = cfg.find_orientations.use_quaternion_grid
         quats = np.loadtxt(qgrid_f).T
         logger.info("Using %s for full quaternion search", qgrid_f)
+        hkl_ids = None
     except (IOError, ValueError):
         # or doing a seeded search?
         logger.info("Defaulting to seeded search")
         hkl_seeds = cfg.find_orientations.seed_search.hkl_seeds
+        hkl_ids = [eta_ome.planeData.hklDataList[i]['hklID'] for i in hkl_seeds]
         hklseedstr = ', '.join(
             [str(i) for i in eta_ome.planeData.hkls.T[hkl_seeds]]
             )
@@ -337,16 +367,41 @@ def find_orientations(cfg, hkls=None, profile=False):
         )
     np.savetxt(os.path.join(cfg.working_dir, 'completeness.dat'), compl)
 
+    ##########################################################
+    ##   Simulate N random grains to get neighborhood size  ##
+    ##########################################################
+    if hkl_ids is not None:
+        ngrains = 10
+        rand_q = mutil.unitVector(np.random.randn(4, ngrains))
+        rand_e = np.tile(2.*np.arccos(rand_q[0, :]), (3, 1)) \
+          * mutil.unitVector(rand_q[1:, :])
+        num_seed_refls = np.zeros(ngrains)
+        for i in range(ngrains):
+            grain_params = np.hstack([rand_e[:, i],
+                                      xf.zeroVec.flatten(),
+                                      xf.vInv_ref.flatten()
+                                      ])
+            sim_results = simulateGVecs(pd,
+                                        detector_params,
+                                        grain_params,
+                                        ome_range=ome_range,
+                                        ome_period=np.radians(cfg.find_orientations.omega.period),
+                                        eta_range=np.radians(cfg.find_orientations.eta.range),
+                                        panel_dims=panel_dims,
+                                        pixel_pitch=cfg.instrument.detector.pixels.size,
+                                        distortion=distortion,
+                                        )
+            num_seed_refls[i] = np.sum([sum(sim_results[0] == hkl_id) for hkl_id in hkl_ids])
+            pass
+        min_samples = np.floor(np.average(num_seed_refls))
+    else:
+        min_samples = 1
+        
     # cluster analysis to identify orientation blobs, the final output:
-    qbar, cl = run_cluster(compl, quats, pd.getQSym(), cfg)
+    qbar, cl = run_cluster(compl, quats, pd.getQSym(), cfg, min_pts=min_pts)
     np.savetxt(
         os.path.join(cfg.working_dir, 'accepted_orientations.dat'),
         qbar.T,
         fmt="%.18e",
         delimiter="\t"
         )
-
-    # obselete # # do the peak extraction now?
-    # obselete # if cfg.find_orientations.extract_measured_g_vectors:
-    # obselete #     raise NotImplementedError('TODO: implement extract gvecs')
-    # obselete #     #extract_measured_g_vectors(cfg)
