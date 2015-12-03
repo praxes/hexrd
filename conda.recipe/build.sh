a148c559cf3646eabdbce081e347fcc38192a8d6
Darren Dale <dsdale24@gmail.com>
2015-12-03 16:49:44 -0500

    conforms to new conda versioning scheme

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,3 +1,10 @@
+#git describe --tags --dirty | sed -r 's/v//' | sed -r 's/-/.post/'\
+#| sed -r 's/-/./g' > $SRC_DIR/__conda_version__.txt
+
+git describe --tags --dirty > $SRC_DIR/__conda_version__.txt
+$PYTHON $RECIPE_DIR/format_version.py $SRC_DIR/__conda_version__.txt
+
+
 rm -rf build
 
 $PYTHON setup.py install --old-and-unmanageable
---
+++ conda.recipe/format_version.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+fn = sys.argv[1]
+with open(fn) as f:
+    s = f.read().lstrip('v').replace('-', '.post', 1).replace('-', '.')
+with open(fn, 'w') as f:
+    f.write(s)
+
