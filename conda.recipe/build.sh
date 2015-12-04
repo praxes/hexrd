0e98db8c6ab1a3e0f9e85052735925179a75333e
Darren Dale <dsdale24@gmail.com>
2015-12-04 10:26:56 -0500

    supports new conda versioning scheme on windows

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,9 +1,5 @@
-#git describe --tags --dirty | sed -r 's/v//' | sed -r 's/-/.post/'\
-#| sed -r 's/-/./g' > $SRC_DIR/__conda_version__.txt
-
 git describe --tags --dirty > $SRC_DIR/__conda_version__.txt
 $PYTHON $RECIPE_DIR/format_version.py $SRC_DIR/__conda_version__.txt
-
 
 rm -rf build
 
