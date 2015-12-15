4abc40615affe8a84d48d8bb5368e952872a9902
Darren Dale <dsdale24@gmail.com>
2015-12-15 14:50:42 -0500

    updates versioneer

--- conda.recipe/format_version.py
+++ conda.recipe/format_version.py
@@ -3,7 +3,6 @@ import sys
 
 fn = sys.argv[1]
 with open(fn) as f:
-    s = f.read().lstrip('v').replace('-', '.post', 1).replace('-', '.')
+    s = f.read().lstrip('v').replace('-', '+', 1).replace('-', '.')
 with open(fn, 'w') as f:
     f.write(s)
-
