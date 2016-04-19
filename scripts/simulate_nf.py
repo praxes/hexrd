579908a1012651cdd3655ca9226ac2d3353107fc
Oscar Villellas <oscar.villellas@continuum.io>
2016-04-19 15:47:57 +0200

    some refactors to simulate_nf

--- scripts/simulate_nf.py
+++ scripts/simulate_nf.py
@@ -4,10 +4,8 @@ Created on Wed Feb 24 11:09:20 2016
 
 @author: bernier2
 """
+import sys
 import yaml
-
-import imageio
-
 import progressbar
 
 import numpy as np
@@ -19,24 +17,28 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 
 from hexrd import matrixutil as mutil
 
-from hexrd.gridutil import cellIndices, cellConnectivity, cellCentroids, compute_areas
+# from hexrd.gridutil import cellIndices, cellConnectivity, cellCentroids, compute_areas
+import hexrd.gridutil as gridutil
 
 from hexrd.xrd import rotations as rot
 from hexrd.xrd import material
 from hexrd.xrd.crystallography import processWavelength
 from hexrd.xrd.symmetry import applySym
 from hexrd.xrd import transforms as xf
-from hexrd.xrd.transforms_CAPI import anglesToGVec, \
-                                      makeRotMatOfExpMap, makeDetectorRotMat, makeOscillRotMat, \
+from hexrd.xrd.transforms_CAPI import anglesToGVec, makeRotMatOfExpMap, \
+                                      makeDetectorRotMat, makeOscillRotMat, \
                                       gvecToDetectorXY, detectorXYToGvec
-from hexrd.xrd.xrdutil import angularPixelSize, make_reflection_patches, \
-                              simulateGVecs, _project_on_detector_plane, \
-                              _fetch_hkls_from_planedata
+#from hexrd.xrd.xrdutil import angularPixelSize, make_reflection_patches, \
+#                              simulateGVecs, _project_on_detector_plane, \
+#                              _fetch_hkls_from_planedata
+import hexrd.xrd.xrdutil as xrdutil
 
 from hexrd.xrd.crystallography import processWavelength
 from hexrd import valunits
-
+from hexrd.utils import profiler
+import numba
 from progressbar import ProgressBar, Percentage, Bar
+
 
 USE_NUMBA = True
 
@@ -145,114 +147,126 @@ pd.tThMax = np.amax(pixel_tth)
 #==============================================================================
 # %% DIFFRACTION SIMULATION
 #==============================================================================
-pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=len(grain_params)).start()
-#image_stack = [sparse.coo_matrix((nrows, ncols), dtype=bool) for i in range(nframes)]
-image_stack = np.zeros((nframes, nrows, ncols), dtype=bool)
-for i in range(len(grain_params)):
-    sim_results = simulateGVecs(pd,
-                                detector_params,
-                                grain_params[i],
-                                panel_dims=panel_dims,
-                                pixel_pitch=pixel_size,
-                                ome_range=ome_range,
-                                ome_period=ome_period,
-                                distortion=None)
-    valid_ids, valid_hkl, valid_ang, valid_xy, ang_ps = sim_results
-    j_pix = cellIndices(x_col_edges, valid_xy[:, 0])
-    i_pix = cellIndices(y_row_edges, valid_xy[:, 1])
-    k_frame = cellIndices(ome_edges, valid_ang[:, 2])
-    
-    # assign intensity
-    for j, k in enumerate(k_frame):
-        image_stack[k][i_pix[j], j_pix[j]] = True
-    pbar.update(i+1)
-    pass
-pbar.finish()
+def simulate_diffractions(grain_params):
+    pbar = ProgressBar(widgets=[Percentage(), Bar()],
+                       maxval=len(grain_params)).start()
 
+    image_stack = np.zeros((nframes, nrows, ncols), dtype=bool)
+    for i in range(len(grain_params)):
+        sim_results = xrdutil.simulateGVecs(pd,
+                                            detector_params,
+                                            grain_params[i],
+                                            panel_dims=panel_dims,
+                                            pixel_pitch=pixel_size,
+                                            ome_range=ome_range,
+                                            ome_period=ome_period,
+                                            distortion=None)
+        valid_ids, valid_hkl, valid_ang, valid_xy, ang_ps = sim_results
+        j_pix = gridutil.cellIndices(x_col_edges, valid_xy[:, 0])
+        i_pix = gridutil.cellIndices(y_row_edges, valid_xy[:, 1])
+        k_frame = gridutil.cellIndices(ome_edges, valid_ang[:, 2])
 
-#==============================================================================
-# %% EXPORT
-#==============================================================================
-#fid = open('/Users/bernier2/Desktop/gold_cubes.ge', 'wb')
-#fid.seek(8192)
-#fid.write(np.frombuffer(image_stack.flatten(), dtype=bool))
-#fid.close()
-np.save('gold_cubes.npy', image_stack)
+        # assign intensity
+        for j, k in enumerate(k_frame):
+            image_stack[k][i_pix[j], j_pix[j]] = True
+        pbar.update(i+1)
+        pass
+    pbar.finish()
+    #==========================================================================
+    # %% EXPORT
+    #==========================================================================
+    np.save('gold_cubes.npy', image_stack)
+    return image_stack
 
 #==============================================================================
 # %% ORIENTATION TESTING
 #==============================================================================
+def test_orientations(image_stack):
+   panel_dims_expanded = [(-10, -10), (10, 10)]
+   panel_buffer = 0.05 # mm
 
-panel_dims_expanded = [(-10, -10), (10, 10)]
-panel_buffer = 0.05 # mm
+   # first evaluate diffraction angles from orientation list (fixed)
+   all_angles = []
+   for i in range(n_grains):
+       gparams = np.hstack([exp_maps[i, :].flatten(), ref_gparams])
+       sim_results = simulateGVecs(pd,
+                                   detector_params,
+                                   gparams,
+                                   panel_dims=panel_dims_expanded,
+                                   pixel_pitch=pixel_size,
+                                   ome_range=ome_range,
+                                   ome_period=ome_period,
+                                   distortion=None)
+       all_angles.append(sim_results[2])
+       pass
 
-# first evaluate diffraction angles from orientation list (fixed)
-all_angles = []
-for i in range(n_grains):
-    gparams = np.hstack([exp_maps[i, :].flatten(), ref_gparams])
-    sim_results = simulateGVecs(pd,
-                                detector_params,
-                                gparams,
-                                panel_dims=panel_dims_expanded,
-                                pixel_pitch=pixel_size,
-                                ome_range=ome_range,
-                                ome_period=ome_period,
-                                distortion=None)
-    all_angles.append(sim_results[2])
-    pass
+   # form test grid and make main loop over spatial coordinates.  
+   #   ** base 5-micron grid for 250x250x250 micron search space
+   cvec_s = 0.001*np.arange(-250, 251)[::5]
+   Xs, Ys, Zs = np.meshgrid(cvec_s, cvec_s, cvec_s)
+   test_crds = np.vstack([Xs.flatten(), Ys.flatten(), Zs.flatten()]).T
 
-# form test grid and make main loop over spatial coordinates.  
-#   ** base 5-micron grid for 250x250x250 micron search space
-cvec_s = 0.001*np.arange(-250, 251)[::5]
-Xs, Ys, Zs = np.meshgrid(cvec_s, cvec_s, cvec_s)
-test_crds = np.vstack([Xs.flatten(), Ys.flatten(), Zs.flatten()]).T
+   # biggest projected diameter for 5 micron cube
+   max_diameter = np.sqrt(3)*0.005
 
-# biggest projected diameter for 5 micron cube
-max_diameter = np.sqrt(3)*0.005
+   row_dilation = np.ceil( 0.5*max_diameter/float(pixel_size[0]) )
+   col_dilation = np.ceil( 0.5*max_diameter/float(pixel_size[1]) )
 
-row_dilation = np.ceil( 0.5*max_diameter/float(pixel_size[0]) )
-col_dilation = np.ceil( 0.5*max_diameter/float(pixel_size[1]) )
+   i_dil, j_dil = np.meshgrid(np.arange(-row_dilation, row_dilation + 1),
+                              np.arange(-col_dilation, col_dilation + 1))
+   i_dil = np.array([i_dil.flatten()], dtype=int)
+   j_dil = np.array([j_dil.flatten()], dtype=int)
 
-i_dil, j_dil = np.meshgrid(np.arange(-row_dilation, row_dilation + 1),
-                           np.arange(-col_dilation, col_dilation + 1))
-i_dil = np.array([i_dil.flatten()], dtype=int)
-j_dil = np.array([j_dil.flatten()], dtype=int)
+   # now the grand loop...
+   confidence = np.zeros((n_grains, len(test_crds)))
+   bMat = pd.latVecOps['B']
+   full_hkls = _fetch_hkls_from_planedata(pd)
+   for icrd in range(len(test_crds)):
+       for igrn in range(n_grains):
+           det_xy, rMat_ss = _project_on_detector_plane(full_hkls[:, 1:], all_angles[igrn], bMat,
+                                                        rMat_d, rMat_c[igrn], chi,
+                                                        tVec_d, test_crds[icrd, :], tVec_s, 
+                                                        distortion)
 
-# now the grand loop...
-confidence = np.zeros((n_grains, len(test_crds)))
-bMat = pd.latVecOps['B']
-full_hkls = _fetch_hkls_from_planedata(pd)
-for icrd in range(len(test_crds)):
-    for igrn in range(n_grains):
-        det_xy, rMat_ss = _project_on_detector_plane(full_hkls[:, 1:], all_angles[igrn], bMat,
-                                                     rMat_d, rMat_c[igrn], chi,
-                                                     tVec_d, test_crds[icrd, :], tVec_s, 
-                                                     distortion)
+           # find on spatial extent of detector
+           xTest = np.logical_and(det_xy[:, 0] >= panel_dims[0][0] + panel_buffer,
+                                  det_xy[:, 0] <= panel_dims[1][0] - panel_buffer)
+           yTest = np.logical_and(det_xy[:, 1] >= panel_dims[0][1] + panel_buffer,
+                                  det_xy[:, 1] <= panel_dims[1][1] - panel_buffer)
 
-        # find on spatial extent of detector
-        xTest = np.logical_and(det_xy[:, 0] >= panel_dims[0][0] + panel_buffer,
-                               det_xy[:, 0] <= panel_dims[1][0] - panel_buffer)
-        yTest = np.logical_and(det_xy[:, 1] >= panel_dims[0][1] + panel_buffer,
-                               det_xy[:, 1] <= panel_dims[1][1] - panel_buffer)
+           onDetector = np.where(np.logical_and(xTest, yTest))[0]
 
-        onDetector = np.where(np.logical_and(xTest, yTest))[0]
-                                                             
-        # pick who's valid
-        row_indices = cellIndices(y_row_edges, det_xy[onDetector, 1])
-        col_indices = cellIndices(x_col_edges, det_xy[onDetector, 0])
-        frame_indices = cellIndices(ome_edges, all_angles[igrn][onDetector, 2])
+           # pick who's valid
+           row_indices = cellIndices(y_row_edges, det_xy[onDetector, 1])
+           col_indices = cellIndices(x_col_edges, det_xy[onDetector, 0])
+           frame_indices = cellIndices(ome_edges, all_angles[igrn][onDetector, 2])
 
-        # perform check
-        tmp_confidence = np.zeros(len(frame_indices), dtype=bool)
-        for iref, indices in enumerate(zip(frame_indices, row_indices, col_indices)):
-            i_sup = indices[1] + i_dil
-            j_sup = indices[2] + j_dil
-            idx_mask = np.where(
-                np.logical_and(np.logical_and(i_sup >= 0, i_sup < nrows),
-                               np.logical_and(j_sup >= 0, j_sup < ncols))
-                               )[0]
-            tmp_confidence[iref] = np.any(image_stack[indices[0]][i_sup[idx_mask], j_sup[idx_mask]])
-            pass
-        confidence[igrn, icrd] = sum(tmp_confidence)/float(len(tmp_confidence))
-        pass
-    pass
+           # perform check
+           tmp_confidence = np.zeros(len(frame_indices), dtype=bool)
+           for iref, indices in enumerate(zip(frame_indices, row_indices, col_indices)):
+               i_sup = indices[1] + i_dil
+               j_sup = indices[2] + j_dil
+               idx_mask = np.where(
+                   np.logical_and(np.logical_and(i_sup >= 0, i_sup < nrows),
+                                  np.logical_and(j_sup >= 0, j_sup < ncols))
+                                  )[0]
+               tmp_confidence[iref] = np.any(image_stack[indices[0]][i_sup[idx_mask], j_sup[idx_mask]])
+               pass
+           confidence[igrn, icrd] = sum(tmp_confidence)/float(len(tmp_confidence))
+           pass
+       pass
+
+
+def main():
+    image_stack = simulate_diffractions(grain_params)
+    #test_orientations(image_stack)
+
+
+if __name__=='__main__':
+    if len(sys.argv) > 1:
+        profiler.instrument_all(sys.argv[1:])
+
+    main()
+
+    if len(sys.argv) > 0:
+        profiler.dump_results(sys.argv[1:])
