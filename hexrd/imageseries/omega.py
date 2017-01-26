09129546c66b78667f711f2038c819d8183f6a8e
Donald Boyce <donald.e.boyce@gmail.com>
2017-01-26 13:00:57 -0500

    added omega_to_frame and omega_ranges_to_frames methods

--- hexrd/imageseries/omega.py
+++ hexrd/imageseries/omega.py
@@ -11,6 +11,7 @@ OMEGA_KEY = 'omega'
 class OmegaImageSeries(ImageSeries):
     """ImageSeries with omega metadata"""
     DFLT_TOL = 1.0e-6
+    TAU = 360
 
     def __init__(self, ims):
         """This class is initialized with an existing imageseries"""
@@ -46,12 +47,23 @@ class OmegaImageSeries(ImageSeries):
             omlast = om[f, 1]
         starts.append(nf)
 
+        nw = len(starts) - 1
+        nf0 = 0
+        self._wedge_om = np.zeros((nw, 3))
+        self._wedge_f = np.zeros((nw, 2), dtype=int)
         self._omegawedges = OmegaWedges(nf)
-        for s in range(len(starts) - 1):
+        for s in range(nw):
             ostart = om[starts[s], 0]
             ostop = om[starts[s + 1] - 1, 1]
             steps = starts[s+1] - starts[s]
             self._omegawedges.addwedge(ostart, ostop, steps)
+            #
+            delta = (ostop - ostart)/steps
+            self._wedge_om[s, :] = (ostart, ostop, delta)
+            self._wedge_f[s, 0] = nf0
+            self._wedge_f[s, 1] = steps
+            nf0 += steps
+        assert(nf0 == nf)
 
     @property
     def omega(self):
@@ -72,6 +84,48 @@ class OmegaImageSeries(ImageSeries):
         delta = (d['ostop'] - d['ostart'])/d['nsteps']
         d.update(delta=delta)
         return d
+
+    def omega_to_frame(self, om):
+        """Return frame and wedge which includes given omega, -1 if not found"""
+        f = -1
+        w = -1
+        f0 = 0
+        for i in range(len(self._wedge_om)):
+            omin = self._wedge_om[i, 0]
+            omax = self._wedge_om[i, 1]
+            omcheck = omin + np.mod(om - omin, self.TAU)
+            if omcheck < omax:
+                odel = self._wedge_om[i, 2]
+                f = self._wedge_f[i,0] + int(np.floor(omcheck - omin)/odel)
+                w = i
+                break
+
+        return f, w
+
+    def omegarange_to_frames(self, omin, omax):
+        """Return list of frames for range of omegas"""
+        noframes = ()
+        f0, w0 = self.omega_to_frame(omin)
+        if w0 < 0:
+            return noframes
+        f1, w1 = self.omega_to_frame(omax)
+        if w1 < 0:
+            return noframes
+
+        # if same wedge, require frames be increasing
+        if (w0 == w1) and (f1 > f0):
+            return range(f0, f1+1)
+
+        # case: adjacent wedges with 2pi jump in omega
+        w0max = self._wedge_om[w0, 1]
+        w1min = self._wedge_om[w1, 0]
+
+        if np.mod(np.abs(w1min - w0max), self.TAU) < self.DFLT_TOL:
+            r0 = range(f0, self._wedge_f[w0, 0] + self._wedge_f[w0, 1])
+            r1 = range(self._wedge_f[w1, 0], f1 + 1)
+            return r0 + r1
+
+        return noframes
 
 
 class OmegaWedges(object):
