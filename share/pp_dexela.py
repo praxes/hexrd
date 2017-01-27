9ac3b02cefc125c5e821fea9063d6802e60f64db
Donald Boyce <donald.e.boyce@gmail.com>
2017-01-27 16:36:59 -0500

    fixed frame-cache write (cache filename in yaml file); cleaned up pp_dexela

--- share/pp_dexela.py
+++ share/pp_dexela.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 from hexrd import imageseries
 
@@ -11,25 +12,28 @@ class PP_Dexela(object):
     RAWPATH = '/imageseries'
     DARKPCTILE = 50
 
-    def __init__(self, fname, omw, frame_start=0):
+    def __init__(self, fname, omw, flips, frame_start=0):
         """Constructor for PP_Dexela"""
         #
 	self.fname = fname
         self.omwedges = omw
+        self.flips = flips
         self.frame_start = frame_start
         self.use_frame_list = (self.frame_start > 0)
         self.raw = imageseries.open(self.fname, self.RAWFMT, path=self.RAWPATH)
         self._dark = None
 
+        print 'On Init: ', self.nframes, self.fname, self.omwedges.nframes,\
+          len(self.raw)
         return
 
     @property
     def oplist(self):
-        return [('dark', self.dark ), ('flip', 't'), ('flip', 'hv') ]
+        return [('dark', self.dark)] + self.flips
 
     @property
     def framelist(self):
-        return range(self.frame_start, self.nframes+self.frame_start)
+        return range(self.frame_start, self.nframes)
     #
     # ============================== API
     #
@@ -53,10 +57,12 @@ class PP_Dexela(object):
     def dark(self, nframes=50):
         """build and return dark image"""
         if self._dark is None:
-            print "building dark images using %s frames (may take a while) ... " % nframes
+            usenframes = min(nframes, self.nframes)
+            print "building dark images using %s frames (may take a while)"\
+            " ... " % usenframes
             start = time.clock()
             self._dark = imageseries.stats.percentile(
-                    self.raw, self.DARKPCTILE, nframes=nframes
+                    self.raw, self.DARKPCTILE, nframes=usenframes
             )
             elapsed = (time.clock() - start)
             print "done building background (dakr) image: elapsed time is %f seconds" \
@@ -65,12 +71,31 @@ class PP_Dexela(object):
         return self._dark
 
     def save_processed(self, name, threshold):
+        dname = '%s-fcache-dir' % name
+        tcname = '%s-fcache-tmp.yml' % name
         fcname = '%s-fcache.yml' % name
         cache = '%s-cachefile.npz' % name
         omname = '%s-omegas.npy' % name
-        imageseries.write(self.processed(), fcname, self.PROCFMT,
+
+        pname = lambda s: os.path.join(dname, s) # prepend fc directory
+
+        os.mkdir(dname)
+
+        # Steps:
+        # * write frame cache with no omegas to temporary file
+        # * write omegas to file
+        # * modify temporary file to include omegas
+        imageseries.write(self.processed(), pname(tcname), self.PROCFMT,
                   threshold=threshold,
                   cache_file=cache)
-        self.save_omegas(omname)
+        self.save_omegas(pname(omname))
+        # modify yaml
+        with open(pname(tcname), 'r') as f:
+            s = f.read()
+        m0 = 'meta: {}'
+        m1 = 'meta:\n  omega: ! load-numpy-array %s' % omname
+        with open(pname(fcname), 'w') as f:
+            f.write(s.replace(m0, m1))
+        os.remove(pname(tcname))
 
     pass  # end class
--- share/pp_init.py
+++ share/pp_init.py
@@ -1,14 +1,24 @@
-from hexrd.imageseries import omega
-from pp_dexla import PP_Dexela
+import os
 
-raw_fname = '/nfs/chess/raw/current/f2/shade-560-1/LSHR-6/%d/ff/ff2_%05d.h5'
+from hexrd.imageseries import omega
+from pp_dexela import PP_Dexela
+
+CHESS_BASE = '/nfs/chess/raw/current/f2/shade-560-1/LSHR-6'
+CHESS_TMPL = '/%d/ff/ff2_%05d.h5'
+
+def h5name(scan, file, base=CHESS_BASE):
+    path = CHESS_TMPL % (scan, file)
+    return os.path.join(base, path)
+
+# ==================== Inputs (should not need to alter above this line)
+
 raw_scannumber = 32
 raw_filenumber = 35
 
-input_name = raw_fname %(raw_scannumber, raw_filenumber)
-output_name = input_name.split('/')[-1].split('.')[0]
+flips = [('flip', 't'), ('flip', 'hv') ]
 
 nframes = 1440
+
 ostart = 0
 ostep = 0.25
 fstart = 5
@@ -16,10 +26,12 @@ threshold = 150
 
 # ==================== End Inputs (should not need to alter below this line)
 
+input_name = h5name(raw_scannumber, raw_filenumber)
+output_name = input_name.split('/')[-1].split('.')[0]
+
 ostop = ostart + nframes*ostep
 omw = omega.OmegaWedges(nframes)
 omw.addwedge(ostart, ostop, nframes)
 
-
-ppd = PP_Dexela(input_name, omw, frame_start=fstart)
+ppd = PP_Dexela(input_name, omw, flips, frame_start=fstart)
 ppd.save_processed(output_name, threshold)
