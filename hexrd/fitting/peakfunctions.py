add masking, fix fitting

hexrd/fitting/peakfunctions.py
@@ -1,24 +1,24 @@
 # ============================================================
-# Copyright (c) 2012, Lawrence Livermore National Security, LLC. 
-# Produced at the Lawrence Livermore National Laboratory. 
-# Written by Joel Bernier <bernier2@llnl.gov> and others. 
-# LLNL-CODE-529294. 
+# Copyright (c) 2012, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by Joel Bernier <bernier2@llnl.gov> and others.
+# LLNL-CODE-529294.
 # All rights reserved.
-# 
+#
 # This file is part of HEXRD. For details on dowloading the source,
 # see the file COPYING.
-# 
+#
 # Please also see the file LICENSE.
-# 
+#
 # This program is free software; you can redistribute it and/or modify it under the
 # terms of the GNU Lesser General Public License (as published by the Free Software
 # Foundation) version 2.1 dated February 1999.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY 
-# or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the 
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this program (see file LICENSE); if not, write to
 # the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
@@ -37,18 +37,18 @@ def _unit_gaussian(p,x):#Split the unit gaussian so this can be called for 2d an
     """
     Required Arguments:
     p -- (m) [x0,FWHM]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
 
 
     x0=p[0]
     FWHM=p[1]
-    sigma=FWHM/gauss_width_fact 
-    
+    sigma=FWHM/gauss_width_fact
+
     f=np.exp(-(x-x0)**2/(2.*sigma**2.))
     return f
 
@@ -56,14 +56,14 @@ def _gaussian1d_no_bg(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
     A=p[0]
-    f=A*_unit_gaussian(p[[1,2]],x)    
+    f=A*_unit_gaussian(p[[1,2]],x)
     return f
 
 
@@ -71,64 +71,64 @@ def gaussian1d(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM,c0,c1]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
     bg0=p[3]
-    bg1=p[4]  
-    
-    f=_gaussian1d_no_bg(p[:3],x)+bg0+bg1*x    
-   
+    bg1=p[4]
+
+    f=_gaussian1d_no_bg(p[:3],x)+bg0+bg1*x
+
     return f
-    
-    
+
+
 def _gaussian1d_no_bg_deriv(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     d_mat -- (3 x n) ndarray of derivative values at positions x
-    """ 
+    """
 
 
     x0=p[1]
     FWHM=p[2]
-    
-    sigma=FWHM/gauss_width_fact 
+
+    sigma=FWHM/gauss_width_fact
 
     dydx0=_gaussian1d_no_bg(p,x)*((x-x0)/(sigma**2.))
-    dydA=_unit_gaussian(p[[1,2]],x) 
+    dydA=_unit_gaussian(p[[1,2]],x)
     dydFWHM=_gaussian1d_no_bg(p,x)*((x-x0)**2./(sigma**3.))/gauss_width_fact
-    
+
     d_mat=np.zeros((len(p),len(x)))
-    
+
     d_mat[0,:]=dydA
     d_mat[1,:]=dydx0
     d_mat[2,:]=dydFWHM
-    
+
     return d_mat
-    
-def gaussian1d_deriv(p,x): 
+
+def gaussian1d_deriv(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM,c0,c1]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     d_mat -- (5 x n) ndarray of derivative values at positions x
-    """ 
+    """
 
 
     d_mat=np.zeros((len(p),len(x)))
     d_mat[0:3,:]=_gaussian1d_no_bg_deriv(p[0:3],x)
-    d_mat[3,:]=1.    
+    d_mat[3,:]=1.
     d_mat[4,:]=x
-    
+
     return d_mat
 
 
@@ -138,16 +138,16 @@ def _unit_lorentzian(p,x):#Split the unit function so this can be called for 2d 
     """
     Required Arguments:
     p -- (m) [x0,FWHM]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
     x0=p[0]
     FWHM=p[1]
-    gamma=FWHM/lorentz_width_fact   
-    
+    gamma=FWHM/lorentz_width_fact
+
     f= gamma**2 / ((x-x0)**2 + gamma**2)
     return f
 
@@ -155,32 +155,32 @@ def _lorentzian1d_no_bg(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
     A=p[0]
-    f= A*_unit_lorentzian(p[[1,2]],x) 
-   
+    f= A*_unit_lorentzian(p[[1,2]],x)
+
     return f
-    
+
 def lorentzian1d(p,x):
     """
     Required Arguments:
     p -- (m) [x0,FWHM,c0,c1]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """     
-    
+    """
+
     bg0=p[3]
     bg1=p[4]
 
-    f=_lorentzian1d_no_bg(p[:3],x)+bg0+bg1*x 
-   
+    f=_lorentzian1d_no_bg(p[:3],x)+bg0+bg1*x
+
     return f
 
 
@@ -188,43 +188,43 @@ def _lorentzian1d_no_bg_deriv(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     d_mat -- (3 x n) ndarray of derivative values at positions x
-    """     
-    
+    """
+
     x0=p[1]
     FWHM=p[2]
-    
-    gamma=FWHM/lorentz_width_fact 
+
+    gamma=FWHM/lorentz_width_fact
 
     dydx0=_lorentzian1d_no_bg(p,x)*((2.*(x-x0))/((x-x0)**2 + gamma**2))
-    dydA=_unit_lorentzian(p[[1,2]],x) 
+    dydA=_unit_lorentzian(p[[1,2]],x)
     dydFWHM=_lorentzian1d_no_bg(p,x)*((2.*(x-x0)**2.)/(gamma*((x-x0)**2 + gamma**2)))/lorentz_width_fact
-    
+
     d_mat=np.zeros((len(p),len(x)))
-    d_mat[0,:]=dydA    
+    d_mat[0,:]=dydA
     d_mat[1,:]=dydx0
     d_mat[2,:]=dydFWHM
-    
+
     return d_mat
-    
-def lorentzian1d_deriv(p,x): 
+
+def lorentzian1d_deriv(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM,c0,c1]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     d_mat -- (5 x n) ndarray of derivative values at positions x
-    """ 
+    """
 
     d_mat=np.zeros((len(p),len(x)))
     d_mat[0:3,:]=_lorentzian1d_no_bg_deriv(p[0:3],x)
-    d_mat[3,:]=1.    
+    d_mat[3,:]=1.
     d_mat[4,:]=x
-    
+
     return d_mat
 
 
@@ -233,15 +233,15 @@ def _unit_pvoigt1d(p,x):#Split the unit function so this can be called for 2d an
     """
     Required Arguments:
     p -- (m) [x0,FWHM,n]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
 
-    n=p[2] 
-    
+    n=p[2]
+
     f=(n*_unit_gaussian(p[:2],x)+(1.-n)*_unit_lorentzian(p[:2],x))
     return f
 
@@ -249,31 +249,31 @@ def _pvoigt1d_no_bg(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM,n]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
-    A=p[0]    
+    A=p[0]
     f=A*_unit_pvoigt1d(p[[1,2,3]],x)
     return f
 
-def pvoigt1d(p,x):   
+def pvoigt1d(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM,n,c0,c1]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
-    """ 
+    """
 
     bg0=p[4]
     bg1=p[5]
 
-    f=_pvoigt1d_no_bg(p[:4],x)+bg0+bg1*x 
-   
+    f=_pvoigt1d_no_bg(p[:4],x)+bg0+bg1*x
+
     return f
 
 #### 1-D Split Psuedo Voigt Functions
@@ -281,7 +281,7 @@ def _split_pvoigt1d_no_bg(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM-,FWHM+,n-,n+]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
@@ -290,27 +290,27 @@ def _split_pvoigt1d_no_bg(p,x):
     A=p[0]
     x0=p[1]
 
-    f=np.zeros(x.shape[0])    
-    
+    f=np.zeros(x.shape[0])
+
     #Define halves, using gthanorequal and lthan, choice is arbitrary
     xr=x>=x0
-    xl=x<x0      
-            
-    #+  
-    r=np.where(xr)  
-    f[r]=A*_unit_pvoigt1d(p[[1,3,5]],x[r])   
-    
+    xl=x<x0
+
+    #+
+    r=np.where(xr)
+    f[xr]=A*_unit_pvoigt1d(p[[1,3,5]],x[xr])
+
     #-
-    l=np.where(xl)  
-    f[l]=A*_unit_pvoigt1d(p[[1,2,4]],x[l])     
+    l=np.where(xl)
+    f[xl]=A*_unit_pvoigt1d(p[[1,2,4]],x[xl])
 
     return f
 
-def split_pvoigt1d(p,x):   
+def split_pvoigt1d(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,FWHM-,FWHM+,n-,n+,c0,c1]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
@@ -320,8 +320,8 @@ def split_pvoigt1d(p,x):
     bg0=p[6]
     bg1=p[7]
 
-    f=_split_pvoigt1d_no_bg(p[:6],x)+bg0+bg1*x 
-   
+    f=_split_pvoigt1d_no_bg(p[:6],x)+bg0+bg1*x
+
     return f
 
 #### Tanh Step Down
@@ -329,7 +329,7 @@ def tanh_stepdown_nobg(p,x):
     """
     Required Arguments:
     p -- (m) [A,x0,w]
-    x -- (n) ndarray of coordinate positions 
+    x -- (n) ndarray of coordinate positions
 
     Outputs:
     f -- (n) ndarray of function values at positions x
@@ -339,8 +339,8 @@ def tanh_stepdown_nobg(p,x):
     x0=p[1]
     w=p[2]
 
-    f=A*(0.5*(1.-np.tanh((x-x0)/w)))    
-        
+    f=A*(0.5*(1.-np.tanh((x-x0)/w)))
+
 
     return f
 
@@ -353,7 +353,7 @@ def _2d_coord_transform(theta,x0,y0,x,y):
 
     x0prime=np.cos(theta)*x0+np.sin(theta)*y0
     y0prime=-np.sin(theta)*x0+np.cos(theta)*y0
-    
+
     return x0prime, y0prime, xprime, yprime
 
 
@@ -368,13 +368,13 @@ def _gaussian2d_no_bg(p,x,y):
 
     Outputs:
     f -- (n x 0) ndarray of function values at positions (x,y)
-    """ 
+    """
 
-    A=p[0]    
-    f=A*_unit_gaussian(p[[1,3]],x)*_unit_gaussian(p[[2,4]],y)  
+    A=p[0]
+    f=A*_unit_gaussian(p[[1,3]],x)*_unit_gaussian(p[[2,4]],y)
     return f
-    
-    
+
+
 def _gaussian2d_rot_no_bg(p,x,y):
     """
     Required Arguments:
@@ -384,22 +384,22 @@ def _gaussian2d_rot_no_bg(p,x,y):
 
     Outputs:
     f -- (n x o) ndarray of function values at positions (x,y)
-    """ 
+    """
 
-    theta=p[5]    
-  
+    theta=p[5]
+
     x0prime, y0prime, xprime, yprime=_2d_coord_transform(theta,p[1],p[2],x,y)
-    
-    newp=copy.copy(p)#this copy was needed so original parameters set isn't changed     
-    
+
+    newp=copy.copy(p)#this copy was needed so original parameters set isn't changed
+
     newp[1]=x0prime
     newp[2]=y0prime
-    
-    f=_gaussian2d_no_bg(newp[:5],xprime,yprime) 
 
-    return f 
-    
-    
+    f=_gaussian2d_no_bg(newp[:5],xprime,yprime)
+
+    return f
+
+
 def gaussian2d_rot(p,x,y):
     """
     Required Arguments:
@@ -409,14 +409,14 @@ def gaussian2d_rot(p,x,y):
 
     Outputs:
     f -- (n x o) ndarray of function values at positions (x,y)
-    """ 
+    """
 
     bg0=p[6]
     bg1x=p[7]
     bg1y=p[8]
- 
-    f=_gaussian2d_rot_no_bg(p[:6],x,y)+(bg0+bg1x*x+bg1y*y)  
-    return f 
+
+    f=_gaussian2d_rot_no_bg(p[:6],x,y)+(bg0+bg1x*x+bg1y*y)
+    return f
 
 def gaussian2d(p,x,y):
     """
@@ -427,14 +427,14 @@ def gaussian2d(p,x,y):
 
     Outputs:
     f -- (n x o) ndarray of function values at positions (x,y)
-    """ 
+    """
 
     bg0=p[5]
     bg1x=p[6]
     bg1y=p[7]
- 
-    f=_gaussian2d_no_bg(p[:5],x,y)+(bg0+bg1x*x+bg1y*y)  
-    return f 
+
+    f=_gaussian2d_no_bg(p[:5],x,y)+(bg0+bg1x*x+bg1y*y)
+    return f
 
 
 #### 2-D Split Psuedo Voigt Function
@@ -448,37 +448,37 @@ def _split_pvoigt2d_no_bg(p,x,y):
 
     Outputs:
     f -- (n x o) ndarray of function values at positions (x,y)
-    """ 
+    """
 
     A=p[0]
     x0=p[1]
     y0=p[2]
 
 
-    f=np.zeros([x.shape[0],x.shape[1]])    
-    
+    f=np.zeros([x.shape[0],x.shape[1]])
+
     #Define quadrants, using gthanorequal and lthan, choice is arbitrary
     xr=x>=x0
-    xl=x<x0      
+    xl=x<x0
     yr=y>=y0
-    yl=y<y0         
-    
-    #++  
-    q1=np.where(xr & yr)  
-    f[q1]=A*_unit_pvoigt1d(p[[1,4,8]],x[q1])*_unit_pvoigt1d(p[[2,6,10]],y[q1])    
-    
+    yl=y<y0
+
+    #++
+    q1=np.where(xr & yr)
+    f[q1]=A*_unit_pvoigt1d(p[[1,4,8]],x[q1])*_unit_pvoigt1d(p[[2,6,10]],y[q1])
+
     #+-
-    q2=np.where(xr & yl)  
-    f[q2]=A*_unit_pvoigt1d(p[[1,4,8]],x[q2])*_unit_pvoigt1d(p[[2,5,9]],y[q2])  
-    
+    q2=np.where(xr & yl)
+    f[q2]=A*_unit_pvoigt1d(p[[1,4,8]],x[q2])*_unit_pvoigt1d(p[[2,5,9]],y[q2])
+
     #-+
-    q3=np.where(xl & yr)  
-    f[q3]=A*_unit_pvoigt1d(p[[1,3,7]],x[q3])*_unit_pvoigt1d(p[[2,6,10]],y[q3]) 
-    
-    #--    
-    q4=np.where(xl & yl)  
-    f[q4]=A*_unit_pvoigt1d(p[[1,3,7]],x[q4])*_unit_pvoigt1d(p[[2,5,9]],y[q4])     
-    
+    q3=np.where(xl & yr)
+    f[q3]=A*_unit_pvoigt1d(p[[1,3,7]],x[q3])*_unit_pvoigt1d(p[[2,6,10]],y[q3])
+
+    #--
+    q4=np.where(xl & yl)
+    f[q4]=A*_unit_pvoigt1d(p[[1,3,7]],x[q4])*_unit_pvoigt1d(p[[2,5,9]],y[q4])
+
 
     return f
 
@@ -492,21 +492,21 @@ def _split_pvoigt2d_rot_no_bg(p,x,y):
 
     Outputs:
     f -- (n x o) ndarray of function values at positions (x,y)
-    """ 
+    """
 
 
-    theta=p[11]    
-  
+    theta=p[11]
+
     x0prime, y0prime, xprime, yprime=_2d_coord_transform(theta,p[1],p[2],x,y)
-    
-    newp=copy.copy(p)#this copy was needed so original parameters set isn't changed   
-    
+
+    newp=copy.copy(p)#this copy was needed so original parameters set isn't changed
+
     newp[1]=x0prime
     newp[2]=y0prime
-    
-    f=_split_pvoigt2d_no_bg(newp[:11],xprime,yprime) 
 
-    return f 
+    f=_split_pvoigt2d_no_bg(newp[:11],xprime,yprime)
+
+    return f
 
 def split_pvoigt2d_rot(p,x,y):
     """
@@ -517,16 +517,16 @@ def split_pvoigt2d_rot(p,x,y):
 
     Outputs:
     f -- (n x o) ndarray of function values at positions (x,y)
-    """ 
+    """
 
 
     bg0=p[12]
     bg1x=p[13]
     bg1y=p[14]
-    
-    f=_split_pvoigt2d_rot_no_bg(p[:12],x,y)+(bg0+bg1x*x+bg1y*y) 
 
-    return f 
+    f=_split_pvoigt2d_rot_no_bg(p[:12],x,y)+(bg0+bg1x*x+bg1y*y)
+
+    return f
 
 #### 3-D Gaussian Function
 def _gaussian3d_no_bg(p,x,y,z):
@@ -539,13 +539,13 @@ def _gaussian3d_no_bg(p,x,y,z):
 
     Outputs:
     f -- (n x o x q) ndarray of function values at positions (x,y)
-    """ 
+    """
 
-    A=p[0]    
-    f=A*_unit_gaussian(p[[1,4]],x)*_unit_gaussian(p[[2,5]],y)*_unit_gaussian(p[[3,6]],z)    
+    A=p[0]
+    f=A*_unit_gaussian(p[[1,4]],x)*_unit_gaussian(p[[2,5]],y)*_unit_gaussian(p[[3,6]],z)
     return f
-    
-    
+
+
 
 
 def gaussian3d(p,x,y,z):
@@ -558,12 +558,12 @@ def gaussian3d(p,x,y,z):
 
     Outputs:
     f -- (n x o x q) ndarray of function values at positions (x,y,z)
-    """ 
+    """
 
     bg0=p[7]
     bg1x=p[8]
     bg1y=p[9]
     bg1z=p[10]
- 
-    f=_gaussian3d_no_bg(p[:5],x,y)+(bg0+bg1x*x+bg1y*y+bg1z*z)  
-    return f 
+
+    f=_gaussian3d_no_bg(p[:5],x,y)+(bg0+bg1x*x+bg1y*y+bg1z*z)
+    return f

hexrd/instrument.py
@@ -459,7 +459,8 @@ class HEDMInstrument(object):
 
     def extract_line_positions(self, plane_data, imgser_dict,
                                tth_tol=None, eta_tol=1., npdiv=2,
-                               collapse_tth=False, do_interpolation=True):
+                               collapse_eta=True, collapse_tth=False,
+                               do_interpolation=True):
         """
         TODO: handle wedge boundaries
 
@@ -576,7 +577,10 @@ class HEDMInstrument(object):
                             patch_data[i_p, j_p] = np.sum(tmp)
                             # ims_data.append(np.sum(tmp))
                         else:
-                            ims_data.append(np.sum(tmp, axis=0))
+                            if collapse_eta:
+                                ims_data.append(np.sum(tmp, axis=0))
+                            else:
+                                ims_data.append(tmp)
                         pass  # close image loop
                     if not collapse_tth:
                         patch_data.append((ang_data, ims_data))
@@ -1010,7 +1014,7 @@ class PlanarDetector(object):
         self._pixel_size_col = pixel_size[1]
 
         self._saturation_level = saturation_level
-        
+
         if panel_buffer is None:
             self._panel_buffer = 25*np.r_[self._pixel_size_col,
                                           self._pixel_size_row]
@@ -1092,7 +1096,7 @@ class PlanarDetector(object):
     def panel_buffer(self, x):
         """if not None, a buffer in mm (x, y)"""
         if x is not None:
-            assert len(x) == 2
+            assert len(x) == 2 or x.ndim == 2
         self._panel_buffer = x
 
     @property
@@ -1270,7 +1274,7 @@ class PlanarDetector(object):
 
         if sat_level is not None:
             d['detector']['saturation_level'] = sat_level
-        
+
         if self.distortion is not None:
             """...HARD CODED DISTORTION! FIX THIS!!!"""
             dist_d = dict(
@@ -1349,11 +1353,28 @@ class PlanarDetector(object):
         xlim = 0.5*self.col_dim
         ylim = 0.5*self.row_dim
         if buffer_edges and self.panel_buffer is not None:
-            xlim -= self.panel_buffer[0]
-            ylim -= self.panel_buffer[1]
-        on_panel_x = np.logical_and(xy[:, 0] >= -xlim, xy[:, 0] <= xlim)
-        on_panel_y = np.logical_and(xy[:, 1] >= -ylim, xy[:, 1] <= ylim)
-        on_panel = np.logical_and(on_panel_x, on_panel_y)
+            if self.panel_buffer.ndim == 2:
+                pix = self.cartToPixel(xy, pixels=True)
+
+                roff = np.logical_or(pix[:, 0] < 0, pix[:, 0] >= self.rows)
+                coff = np.logical_or(pix[:, 1] < 0, pix[:, 1] >= self.cols)
+
+                idx = np.logical_or(roff, coff)
+
+                pix[idx, :] = 0
+
+                on_panel = self.panel_buffer[pix[:, 0], pix[:, 1]]
+                on_panel[idx] = False
+            else:
+                xlim -= self.panel_buffer[0]
+                ylim -= self.panel_buffer[1]
+                on_panel_x = np.logical_and(
+                    xy[:, 0] >= -xlim, xy[:, 0] <= xlim
+                )
+                on_panel_y = np.logical_and(
+                    xy[:, 1] >= -ylim, xy[:, 1] <= ylim
+                )
+                on_panel = np.logical_and(on_panel_x, on_panel_y)
         return xy[on_panel, :], on_panel
 
     def cart_to_angles(self, xy_data):